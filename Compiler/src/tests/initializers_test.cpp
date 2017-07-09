af90ac467b11ff8edce4af18854ae6ee01fc731a
Panzerschrek <AntiPanzerschrek@yandex.ru>
2017-07-09 17:22:12 +0700

    Testy na iniçializaçiju massivov.

--- Compiler/src/tests/initializers_test.cpp
+++ Compiler/src/tests/initializers_test.cpp
@@ -110,12 +110,98 @@ static void ConstructorInitializerForFundamentalTypesTest1()
 	U_ASSERT( 2017.52 == result_value.DoubleVal );
 }
 
+static void ArrayInitializerForFundamentalTypesTest0()
+{
+	static const char c_program_text[]=
+	R"(
+	fn Foo() : i32
+	{
+		let : [ i32, 3u32 ] x[ 42, 34, 785 ];
+		return x[0u32] * x[1u32] - x[2u32];
+	}
+	)";
+
+	llvm::ExecutionEngine* const engine= CreateEngine( BuildProgram( c_program_text ) );
+
+	llvm::Function* function= engine->FindFunctionNamed( "Foo" );
+	U_ASSERT( function != nullptr );
+
+	llvm::GenericValue result_value=
+		engine->runFunction(
+			function,
+			llvm::ArrayRef<llvm::GenericValue>() );
+
+	U_ASSERT( static_cast<uint64_t>( 42 * 34 - 785 ) == result_value.IntVal.getLimitedValue() );
+}
+
+static void ArrayInitializerForFundamentalTypesTest1()
+{
+	// Array initializer with comma at end must works correctly
+	static const char c_program_text[]=
+	R"(
+	fn Foo() : f32
+	{
+		let : [ f32, 3u32 ] x[ 42.5f32, 34.0f32, 785.7f32, ];
+		return x[0u32] * x[1u32] - x[2u32];
+	}
+	)";
+
+	llvm::ExecutionEngine* const engine= CreateEngine( BuildProgram( c_program_text ) );
+
+	llvm::Function* function= engine->FindFunctionNamed( "Foo" );
+	U_ASSERT( function != nullptr );
+
+	llvm::GenericValue result_value=
+		engine->runFunction(
+			function,
+			llvm::ArrayRef<llvm::GenericValue>() );
+
+	U_ASSERT( 42.5f * 34.0f - 785.7f == result_value.FloatVal );
+}
+
+static void TwodimensionalArrayInitializerTest0()
+{
+	static const char c_program_text[]=
+	R"(
+	fn Foo() : i32
+	{
+		let : [ [ i32, 2u32 ], 3u32 ] mat
+			[
+				[ 175, -8 * 5, ],
+				[ 95684, 48 ],
+				[ -14, 2 + 2 * 2 ],
+			];
+		return
+			mat[0u32][0u32] + mat[0u32][1u32] +
+			mat[1u32][0u32] + mat[1u32][1u32] +
+			mat[2u32][0u32] * mat[2u32][1u32];
+	}
+	)";
+
+	llvm::ExecutionEngine* const engine= CreateEngine( BuildProgram( c_program_text ) );
+
+	llvm::Function* function= engine->FindFunctionNamed( "Foo" );
+	U_ASSERT( function != nullptr );
+
+	llvm::GenericValue result_value=
+		engine->runFunction(
+			function,
+			llvm::ArrayRef<llvm::GenericValue>() );
+
+	U_ASSERT(
+		static_cast<uint64_t>( (175) + (-8 * 5) + (95684) + (48) + (-14) * (2 + 2 * 2 ) ) ==
+		result_value.IntVal.getLimitedValue() );
+}
+
 void RunInitializersTest()
 {
 	ExpressionInitializerTest0();
 	ExpressionInitializerTest1();
 	ConstructorInitializerForFundamentalTypesTest0();
 	ConstructorInitializerForFundamentalTypesTest1();
+	ArrayInitializerForFundamentalTypesTest0();
+	ArrayInitializerForFundamentalTypesTest1();
+	TwodimensionalArrayInitializerTest0();
 }
 
 } // namespace U
