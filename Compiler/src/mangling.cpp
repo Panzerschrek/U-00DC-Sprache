0f77dcf7ddedeb248d9aa41fff3a7d6d5561ce30
Panzerschrek <AntiPanzerschrek@yandex.ru>
2017-08-11 12:21:29 +0700

    Popytka realizovatj sžatije imön.

--- Compiler/src/mangling.cpp
+++ Compiler/src/mangling.cpp
@@ -14,7 +14,40 @@ namespace CodeBuilderPrivate
 // TODO
 // Process scope encoding correctly.
 
-static void GetNamespacePrefix_r( const NamesScope& names_scope, ProgramString& result )
+typedef std::vector<ProgramString> NamesCache;
+
+struct Name final
+{
+	ProgramString full;
+	ProgramString compressed_and escaped;
+};
+
+static size_t GetRepalcement( const NamesCache& names_cache, ProgramString& name )
+{
+	for( const ProgramString& candidate : names_cache )
+		if( name == candidate )
+			return &candidate - names_cache.data();
+
+	return std::numeric_limits<size_t>::max();
+}
+
+static void AddNameToCache( NamesCache& names_cache, ProgramString& name )
+{
+	for( const ProgramString& candidate : names_cache )
+		if( name == candidate )
+			return;
+
+	names_cache.push_back( name );
+}
+
+static ProgramString RetReplacementString( const size_t n )
+{
+	if( n == 0u )
+		return "S_"_SpC;
+	return "S"_SpC + ToProgramString( std::to_string( n - 1u ).c_str() ) + "_"_SpC;
+}
+
+static void GetNamespacePrefix_r( const NamesScope& names_scope, std::vector<ProgramString>& result )
 {
 	const NamesScope* const parent= names_scope.GetParent();
 	if( parent != nullptr )
@@ -23,49 +56,63 @@ static void GetNamespacePrefix_r( const NamesScope& names_scope, ProgramString& 
 	const ProgramString& name= names_scope.GetThisNamespaceName();
 	if( !name.empty() )
 	{
-		result+= ToProgramString( std::to_string( name.size() ).c_str() );
-		result+= name;
+		result.push_back( ToProgramString( std::to_string( name.size() ).c_str() ) + name );
 	}
 }
 
-// Returns true, if name is nested.
-static bool GetNestedName( const NamesScope& parent_scope, const bool need_konst, ProgramString& result )
+static ProgramString GetNestedName( const ProgramString& name, const NamesScope& parent_scope, const bool need_konst, NamesCache& names_cache, bool name_is_func_name= false )
 {
 	// "N" - prefix for all names inside namespaces or classes.
 	// "K" prefix for "this-call" methods with immutable "this".
-	result+= "N"_SpC;
-	if( need_konst )
-		result+= "K"_SpC;
+	// "E" postfix fol all names inside namespaces or classes.
 
-	const size_t size_before_scope= result.size();
-	GetNamespacePrefix_r( parent_scope, result );
-	if( result.size() == size_before_scope )
+	std::vector<ProgramString> result_splitted;
+	GetNamespacePrefix_r( parent_scope, result_splitted );
+	result_splitted.push_back( ToProgramString( std::to_string( name.size() ).c_str() ) + name );
+
+	ProgramString name_combined;
+	const ProgramString konst= need_konst ? "K"_SpC : ""_SpC;
+
+	size_t replacement_n= std::numeric_limits<size_t>::max();
+	size_t replacement_pos= 0u;
+	for( const ProgramString& comp : result_splitted )
 	{
-		if( need_konst )
-			result[ result.size() - 2u ]= result.back();
+		name_combined+= comp;
+		const size_t replacement_candidate= GetRepalcement( names_cache, name_combined );
+		if( replacement_candidate != std::numeric_limits<size_t>::max() )
+		{
+			replacement_n= replacement_candidate;
+			replacement_pos= size_t( &comp - result_splitted.data() );
+		}
 
-		result.pop_back(); // "N" prefix for namespace doesn`t need, because namespace prefix is empty.
-		return false;
+		if( !( name_is_func_name && &comp == &result_splitted.back() ) )
+			AddNameToCache( names_cache, name_combined );
 	}
 
-	return true;
+	if( replacement_n == std::numeric_limits<size_t>::max() )
+	{
+		if( result_splitted.size() == 1u )
+			return name_combined;
+		return "N"_SpC + konst + name_combined + "E"_SpC;
+	}
+
+	if( replacement_pos + 1u == result_splitted.size() )
+		return konst + RetReplacementString( replacement_n );
+	else
+	{
+		name_combined= "N"_SpC + konst + RetReplacementString( replacement_n );
+		for( size_t i= replacement_pos + 1u; i < result_splitted.size(); i++ )
+			name_combined+= result_splitted[i];
+		name_combined+= "E"_SpC;
+
+		return name_combined;
+	}
 }
 
-static void MangleClassImpl(
-	const NamesScope& parent_scope,
-	const ProgramString& class_name,
-	 ProgramString& result )
+static ProgramString GetTypeName_r( const Type& type, NamesCache& names_cache )
 {
-	const bool is_nested= GetNestedName( parent_scope, false, result );
+	ProgramString result;
 
-	result+= ToProgramString( std::to_string( class_name.size() ).c_str() );
-	result+= class_name;
-
-	if( is_nested ) result+= "E"_SpC;
-}
-
-static void GetTypeName_r( const Type& type, ProgramString& result )
-{
 	if( const FundamentalType* const fundamental_type= boost::get<FundamentalType>( &type.one_of_type_kind ) )
 	{
 		switch( fundamental_type->fundamental_type )
@@ -91,14 +138,16 @@ static void GetTypeName_r( const Type& type, ProgramString& result )
 		U_ASSERT( *array_type_ptr != nullptr );
 		result+= "A"_SpC;
 		result+= ToProgramString( std::to_string( (*array_type_ptr)->size ).c_str() );
-		GetTypeName_r( (*array_type_ptr)->type, result );
+		result+= GetTypeName_r( (*array_type_ptr)->type, names_cache );
 	}
 	else if( const ClassPtr* const class_type_ptr= boost::get<ClassPtr>( &type.one_of_type_kind ) )
 	{
 		U_ASSERT( *class_type_ptr != nullptr );
 		const Class& class_type= **class_type_ptr;
-		MangleClassImpl( *class_type.members.GetParent(), class_type.members.GetThisNamespaceName(), result );
+		result+= GetNestedName( class_type.members.GetThisNamespaceName(), *class_type.members.GetParent(), false, names_cache );
 	}
+
+	return result;
 }
 
 std::string MangleFunction(
@@ -109,30 +158,26 @@ std::string MangleFunction(
 {
 	U_ASSERT( !( is_this_call_method && function_type.args.empty() ) );
 
+	NamesCache names_cache;
 	ProgramString result;
 
 	// "_Z" - common prefix for all symbols.
 	result+= "_Z"_SpC;
 
-	const bool is_nested=
-		GetNestedName(
-			parent_scope,
-			is_this_call_method && !function_type.args.front().is_mutable,
-			result );
-
 	if( function_name == Keywords::constructor_ )
 	{
 		// TODO
 		// Itanium ABI requires at least 2  cnstructors - "C1" and "C2". We should generate both.
-		result+= "C1"_SpC;
+		result+= GetNestedName( "C1"_SpC, parent_scope, false, names_cache, true );
 	}
 	else
-	{
-		result+= ToProgramString( std::to_string( function_name.size() ).c_str() );
-		result+= function_name;
-	}
-
-	if( is_nested ) result+= "E"_SpC;
+		result+=
+			GetNestedName(
+				function_name,
+				parent_scope,
+				is_this_call_method && !function_type.args.front().is_mutable,
+				names_cache,
+				true );
 
 	size_t arg_count= function_type.args.size();
 	const Function::Arg* args= function_type.args.data();
@@ -149,7 +194,7 @@ std::string MangleFunction(
 		if( !arg.is_mutable )
 			result+= "K"_SpC;
 
-		GetTypeName_r( arg.type, result );
+		result+= GetTypeName_r( arg.type, names_cache );
 	}
 	if( arg_count == 0u )
 		result+= "v"_SpC;
@@ -161,10 +206,8 @@ std::string MangleClass(
 	const NamesScope& parent_scope,
 	const ProgramString& class_name )
 {
-	ProgramString result;
-	MangleClassImpl( parent_scope, class_name, result );
-
-	return ToStdString( result );
+	NamesCache names_cache;
+	return ToStdString( GetNestedName( class_name, parent_scope, false, names_cache ) );
 }
 
 } // namespace CodeBuilderPrivate
