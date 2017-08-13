ab387e159aa4107508a26018de361aea32ea73fb
Panzerschrek <AntiPanzerschrek@yandex.ru>
2017-08-13 21:29:29 +0700

    Napisan test na peredacu v programmu na Ü-Sprache ssylki na massiv.

--- Compiler/test_programs/cpp_linkage.cpp
+++ Compiler/test_programs/cpp_linkage.cpp
@@ -37,6 +37,8 @@ void TheClass::StaticMethod2()
 	std::cout << "Call StaticMethod2" << std::endl;
 }
 
+void U32ToStr( unsigned int x, signed char (&str)[64] );
+
 int main()
 {
 	// Call some Ü-Sprache functions
@@ -45,6 +47,15 @@ int main()
 	TheClass the_class;
 	the_class.Method();
 	the_class.StaticMethod();
+
+	// Real usable function - convert number to string.
+	signed char str[64];
+	static const unsigned int numbers[]= { 0u, 1u, 5u, 10u, 99u, 100u, 101u, 58u, 586u, 1024u, 1000000000u, 4294967295u };
+	for( unsigned int number : numbers )
+	{
+		U32ToStr( number, str );
+		std::cout << str << std::endl;
+	}
 
 	const double res= Do( 42 );
 	std::cout << "Res: " << res << std::endl;
