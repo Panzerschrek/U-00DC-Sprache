ec3ca90a3cf20b90331235c22822272f9668b2c0
Panzerschrek <AntiPanzerschrek@yandex.ru>
2018-02-03 17:29:42 +0700

    Dobavlen C++ fajl so vhodnoj tockoj dlä Ü programmy.

---
+++ Compiler/data/entry.cpp
@@ -0,0 +1,15 @@
+﻿/* This is simple C++ program, working as launcher for Ü program.
+  Usage - wrote your Ü program, compile it into set of object files, compile it against this launcher.
+  For example:
+    gcc my_ü_program.o entry.cpp -o my_ü_program.exe
+
+  This launcher developed for usage of Ü together with some C++ code. Launcher allows you to use C++ standart library,
+  which initialized normally before C++ "main" function.
+*/
+
+extern int U_Main();
+
+int main()
+{
+	U_Main();
+}
