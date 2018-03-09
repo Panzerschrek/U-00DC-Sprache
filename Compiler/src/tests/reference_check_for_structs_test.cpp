bf648f47a34d8dd7ec5cf64ef7f9840d58429ac4
Panzerschrek <AntiPanzerschrek@yandex.ru>
2018-03-09 21:49:23 +0700

    Dobavlen test na vozvraschenije ssylki na lokaljnuju peremennuju cerez strukturu.

--- Compiler/src/tests/reference_check_for_structs_test.cpp
+++ Compiler/src/tests/reference_check_for_structs_test.cpp
@@ -256,6 +256,29 @@ U_TEST( ReturnReferenceFromArg_Test5 )
 	U_TEST_ASSERT( error.file_pos.line == 9u );
 }
 
+U_TEST( ReturnReferenceToLocalVariableInsideStruct )
+{
+	static const char c_program_text[]=
+	R"(
+		struct S { i32& x; }
+
+		fn Foo() : S
+		{
+			var i32 x= 0;
+			var S s{ .x= x };
+			return s;
+		}
+	)";
+
+	const ICodeBuilder::BuildResult build_result= BuildProgramWithErrors( c_program_text );
+
+	U_TEST_ASSERT( !build_result.errors.empty() );
+	const CodeBuilderError& error= build_result.errors.front();
+
+	U_TEST_ASSERT( error.code == CodeBuilderErrorCode::ReturningUnallowedReference );
+	U_TEST_ASSERT( error.file_pos.line == 8u );
+}
+
 U_TEST( GetReturnedReferencePassedThroughArgument_Test0 )
 {
 	static const char c_program_text[]=
