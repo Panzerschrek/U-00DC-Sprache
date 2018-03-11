6c9d50ae9c4b51b962d674f12933645aa48e9f48
Panzerschrek <AntiPanzerschrek@yandex.ru>
2018-03-11 15:43:04 +0700

    Test na peremescenije iz rezuljtata funkçii v argument drugoj funkçii.

--- Compiler/src/tests/temp_variables_moving_test.cpp
+++ Compiler/src/tests/temp_variables_moving_test.cpp
@@ -396,4 +396,57 @@ U_TEST(TempVariablesMovingTest6_MoveInConstructorInitializer)
 		 g_destructors_call_sequence == std::vector<int>( { 9565412 } ) );
 }
 
+U_TEST(TempVariablesMovingTest7_MoveTempVariableReturningFromFunctionToAnotherFunction)
+{
+	TestPrepare();
+
+	static const char c_program_text[]=
+	R"(
+		fn ConstructorCalled(i32 x);
+		fn  DestructorCalled(i32 x);
+
+		class S
+		{
+			i32 x;
+			fn constructor()
+				( x= 0 )
+			{
+				ConstructorCalled(x);
+			}
+			fn constructor( i32 in_x )
+				( x= in_x )
+			{
+				ConstructorCalled(x);
+			}
+			fn constructor( S& imut other )
+				( x= other.x )
+			{
+				ConstructorCalled(x);
+			}
+			fn destructor()
+			{
+				DestructorCalled(x);
+			}
+		}
+
+		fn Bar() : S { return S(65214); }
+		fn Baz( S s ){}
+
+		fn Foo()
+		{
+			Baz(Bar());
+		}
+	)";
+
+	const EnginePtr engine= CreateEngine( BuildProgram( c_program_text ) );
+	llvm::Function* const function= engine->FindFunctionNamed( "_Z3Foov" );
+	U_TEST_ASSERT( function != nullptr );
+
+	engine->runFunction( function, llvm::ArrayRef<llvm::GenericValue>() );
+
+	U_TEST_ASSERT(
+		g_constructirs_call_sequence == std::vector<int>( { 65214 } ) &&
+		 g_destructors_call_sequence == std::vector<int>( { 65214 } ) );
+}
+
 } // namespace U
