a51e94c78a1079c842dcdcd588dd654a76884517
Panzerschrek <AntiPanzerschrek@yandex.ru>
2018-03-30 22:29:59 +0700

    Iz "Python" možno pozvatj postrojenije programmy.

--- Compiler/compiler_lib_py/compiler_lib_py.cpp
+++ Compiler/compiler_lib_py/compiler_lib_py.cpp
@@ -3,13 +3,57 @@
 
 #include <Python.h>
 
+#include "../src/assert.hpp"
 #include "../src/lexical_analyzer.hpp"
 #include "../src/syntax_analyzer.hpp"
 #include "../src/code_builder.hpp"
+#include "../src/source_graph_loader.hpp"
 
-// sprache_compiler_lib.pyd
+namespace U
+{
 
-static PyObject* test_echo( PyObject* self, PyObject* args )
+class SingeFileVfs final : public IVfs
+{
+public:
+
+	SingeFileVfs( ProgramString file_path, const char* const text )
+		: file_path_(file_path), file_text_(text)
+	{}
+
+	virtual boost::optional<LoadFileResult> LoadFileContent( const Path& file_path, const Path& full_parent_file_path ) override
+	{
+		U_UNUSED( full_parent_file_path );
+		if( file_path == file_path_ )
+			return LoadFileResult{ file_path_, DecodeUTF8( file_text_ ) };
+		return boost::none;
+	}
+
+private:
+	const ProgramString file_path_;
+	const char* const file_text_;
+};
+
+void BuildProgram( const char* const text )
+{
+	const ProgramString file_path= "_"_SpC;
+	const SourceGraphPtr source_graph=
+		SourceGraphLoader( std::make_shared<SingeFileVfs>( file_path, text ) ).LoadSource( file_path );
+
+	if( source_graph == nullptr ||
+		!source_graph->lexical_errors.empty() ||
+		!source_graph->syntax_errors.empty() ||
+		!( source_graph->root_node_index < source_graph->nodes_storage.size() ) )
+		return;
+
+	const ICodeBuilder::BuildResult build_result= CodeBuilder().BuildProgram( *source_graph );
+
+	for( const CodeBuilderError& error : build_result.errors )
+		std::cout << error.file_pos.line << ":" << error.file_pos.pos_in_line << " " << ToStdString( error.text ) << "\n";
+}
+
+} // namespace U
+
+static PyObject* TestEcho( PyObject* self, PyObject* args )
 {
 	const char* command= nullptr;
 
@@ -19,9 +63,22 @@ static PyObject* test_echo( PyObject* self, PyObject* args )
 	return Py_None;
 }
 
+static PyObject* BuildProgram( PyObject* self, PyObject* args )
+{
+	const char* program_text= nullptr;
+
+	if( !PyArg_ParseTuple( args, "s", &program_text ) )
+		return nullptr;
+
+	U::BuildProgram( program_text );
+
+	return Py_None;
+}
+
 static PyMethodDef sprace_methods[]=
 {
-	{ "test_echo",  test_echo, METH_VARARGS, "Test echo." },
+	{ "test_echo",  TestEcho, METH_VARARGS, "Test echo." },
+	{ "build_program",  BuildProgram, METH_VARARGS, "Build program." },
 	{ nullptr, nullptr, 0, nullptr }        /* Sentinel */
 };
 
--- Compiler/compiler_lib_py/compiler_lib_py_test.py
+++ Compiler/compiler_lib_py/compiler_lib_py_test.py
@@ -3,7 +3,10 @@ sys.path.append('../../Compiler-build')
 import sprache_compiler_lib
 
 def main():
-	sprache_compiler_lib.test_echo( "Hello, world" )
+	print( "test0" )
+	sprache_compiler_lib.build_program( "fn foo() {}" )
+	print( "test1" )
+	sprache_compiler_lib.build_program( "fn foo() : i32 {}" )
 
 if __name__ == "__main__":
 	sys.exit(main())
