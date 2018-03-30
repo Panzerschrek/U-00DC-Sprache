c072baab493f65481d96f4236a58e94e3cb1a8be
Panzerschrek <AntiPanzerschrek@yandex.ru>
2018-03-30 22:08:29 +0700

    Biblioteka pri sborke nazyvajetsä kak nado.

--- Compiler/compiler_lib_py/compiler_lib_py_test.py
+++ Compiler/compiler_lib_py/compiler_lib_py_test.py
@@ -1,7 +1,6 @@
 import sys
-sys.path.append('C:/lnk/U+00DC-Sprache/Compiler-build')
+sys.path.append('../../Compiler-build')
 import sprache_compiler_lib
-#from 'C:/lnk/U+00DC-Sprache/Compiler-build/sprache_compiler_lib import 'sprache_compiler_lib'
 
 def main():
 	sprache_compiler_lib.test_echo( "Hello, world" )
