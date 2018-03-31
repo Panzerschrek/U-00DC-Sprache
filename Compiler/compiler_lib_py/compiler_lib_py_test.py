38f1c3dfb84ec1504bb0c56045a293f02d3ec73a
Panzerschrek <AntiPanzerschrek@yandex.ru>
2018-03-31 16:17:47 +0700

    Napisana obväzka dlä "Python-testov".

--- Compiler/compiler_lib_py/compiler_lib_py_test.py
+++ Compiler/compiler_lib_py/compiler_lib_py_test.py
@@ -1,4 +1,6 @@
+import inspect
 import sys
+import traceback
 import sprache_compiler_tests_py_lib as tests_lib
 
 class FilePos:
@@ -10,6 +12,8 @@ class FilePos:
 class CodeBuilderError:
 	error_code= ""
 	file_pos= FilePos()
+
+#tests_list= []
 
 
 def ConvertErrors( errors_list ):
@@ -28,9 +32,7 @@ def OkTest():
 	tests_lib.build_program( "fn Foo( i32 x, i32 y ) : f32 { return f32(x * y) + 0.5f; }" )
 
 	call_result= tests_lib.run_function( "_Z3Fooii", 3, 7 )
-	assert( call_result == ( 3 * 7 ) + 0.5 )
-
-	tests_lib.free_program()
+	assert( call_result == ( 3 * 7 ) + 0.50 )
 
 
 def ErrorsTest():
@@ -40,9 +42,27 @@ def ErrorsTest():
 	assert( errors_list[0].file_pos.line == 1 )
 
 
+# Get list of all tests here.
+tests_list = [ obj for name, obj in inspect.getmembers(sys.modules[__name__]) if inspect.isfunction(obj) and obj != ConvertErrors ]
+
 def main():
-	OkTest()
-	ErrorsTest()
+
+	print( "run " + str(len(tests_list)) + " tests" + "\n" )
+	tests_failed= 0
+
+	for test in tests_list:
+		try:
+			test()
+			tests_lib.free_program()
+		except Exception as ex:
+			print( "test " + str(test) + " failed" )
+			traceback.print_exc( file= sys.stdout )
+			print()
+			tests_failed= tests_failed + 1
+			tests_lib.free_program()
+
+	print( str( len(tests_list) - tests_failed ) + " - passed" )
+	print( str(tests_failed) + " - failed" )
 
 
 if __name__ == "__main__":
