d6c7bee482919b5bbc0c03a576ade8f227a704ba
Panzerschrek <AntiPanzerschrek@yandex.ru>
2018-04-17 13:36:51 +0700

    Popolnenije nabora testov na vyvod parametrov šablonnoj funkçii iz dannyh argumentov.

--- Compiler/py_tests/function_templates_test.py
+++ Compiler/py_tests/function_templates_test.py
@@ -78,3 +78,46 @@ def FunctionTemplateBase_Test1():
 	tests_lib.build_program( c_program_text )
 	call_result= tests_lib.run_function( "_Z3Foov" )
 	assert( call_result == 93 )
+
+
+def FunctionTemplateBase_Test2():
+	c_program_text= """
+		template</ type T />
+		struct Box
+		{
+			T boxed;
+		}
+
+		template</ type T />
+		fn Unbox( Box</ T />& box ) : T&
+		{
+			return box.boxed;
+		}
+
+		fn Foo() : f64
+		{
+			var Box</ f64 /> pi_in_box{ .boxed= 3.1415926535 };
+			return Unbox( pi_in_box );  // Must deduce complex argument.
+		}
+	"""
+	tests_lib.build_program( c_program_text )
+	call_result= tests_lib.run_function( "_Z3Foov" )
+	assert( call_result == 3.1415926535 )
+
+
+def FunctionTemplateBase_Test3():
+	c_program_text= """
+		template</ type T />
+		fn Div( T a, T b ) : T
+		{
+			return a / b;
+		}
+
+		fn Foo() : f64
+		{
+			return f64( Div( 48, 6 ) ) + Div( 14.5, 1.0 );  // Must deduce multiple parameters with same template type.
+		}
+	"""
+	tests_lib.build_program( c_program_text )
+	call_result= tests_lib.run_function( "_Z3Foov" )
+	assert( call_result == ( 48.0 / 6.0 + 14.5 / 1.0 ) )
