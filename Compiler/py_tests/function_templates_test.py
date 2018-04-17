710bd23961a3c5a085e03ef242962c763aece509
Panzerschrek <AntiPanzerschrek@yandex.ru>
2018-04-17 10:02:40 +0700

    Dobavlen test na razbor ovjavlenija šablonov funkçij.

---
+++ Compiler/py_tests/function_templates_test.py
@@ -0,0 +1,38 @@
+from py_tests_common import *
+
+def FunctionTemplateDeclaration_Test0():
+	c_program_text= """
+		template</ type T />
+		fn ToVoid( T& t ) : void&
+		{
+			return t;
+		}
+	"""
+	tests_lib.build_program( c_program_text )
+
+
+def FunctionTemplateDeclaration_Test1():
+	c_program_text= """
+		template</ type T, u32 size />
+		fn ZeroFill( [ T, size ] &mut arr )
+		{
+			var u32 i= 0u;
+			while( i < size )
+			{
+				arr[i]= T(0);
+				++i;
+			}
+		}
+	"""
+	tests_lib.build_program( c_program_text )
+
+
+def FunctionTemplateDeclaration_Test2():
+	c_program_text= """
+		class C
+		{
+			template</ type T, type U />
+			fn DoNothing( T& t, U& u ) {}
+		}
+	"""
+	tests_lib.build_program( c_program_text )
