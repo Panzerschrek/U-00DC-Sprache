06085685e5bd376d29ae02b634357491f3bb6b77
Panzerschrek <AntiPanzerschrek@yandex.ru>
2018-08-27 10:15:48 +0700

    Zakostylen zapusk testov pod GNU/Linux.

--- Compiler/tests/tests_common.hpp
+++ Compiler/tests/tests_common.hpp
@@ -7,8 +7,9 @@ namespace Constants
 {
 
 // Hack. Use constant target triple and data layout for all tests.
-const char tests_target_triple_str[]= "i686-pc-windows-gnu";
-const char tests_data_layout_str[]= "e-m:x-p:32:32-i64:64-f80:32-n8:16:32-a:0:32-S32";
+const bool is_32bit= sizeof(void*) == 4u;
+const char* const tests_target_triple_str= is_32bit ? "i686-pc-windows-gnu" : "x86_64-unknown-linux-gnu";
+const char* const tests_data_layout_str= is_32bit ? "e-m:x-p:32:32-i64:64-f80:32-n8:16:32-a:0:32-S32" : "e-m:e-i64:64-f80:128-n8:16:32:64-S128";
 
 } // namespace Constants
 
