d29ecd56d318eb363449b2373efec9a8e58f1970
Panzerschrek <AntiPanzerschrek@yandex.ru>
2018-08-29 15:37:44 +0700

    Ispoljzovana svoja modelj dlä vida funkçij.

--- U_QtCreator_Plugin/uspracheplugin.cpp
+++ U_QtCreator_Plugin/uspracheplugin.cpp
@@ -1,29 +1,13 @@
 #include "uspracheplugin.h"
 #include "uspracheconstants.h"
 
+#include <QObject>
 #include <coreplugin/icore.h>
 #include <coreplugin/icontext.h>
 #include <coreplugin/actionmanager/actionmanager.h>
 #include <coreplugin/actionmanager/command.h>
 #include <coreplugin/actionmanager/actioncontainer.h>
 #include <coreplugin/coreconstants.h>
-
-#include <coreplugin/editormanager/editormanager.h>
-#include <coreplugin/messagemanager.h>
-
-#include <plugins/texteditor/texteditor.h>
-#include <plugins/texteditor/textdocument.h>
-
-#include <utils/treeviewcombobox.h>
-#include <utils/dropsupport.h>
-
-#include <QAction>
-#include <QMessageBox>
-#include <QMainWindow>
-#include <QMenu>
-#include <QTimer>
-#include <QAbstractItemModel>
-#include <QStringListModel>
 
 #include "../Compiler/src/lexical_analyzer.hpp"
 #include "../Compiler/src/program_string.hpp"
@@ -39,228 +23,150 @@ const char g_editor_id[]= "sprache_editor";
 //const char g_mime_type[]= "text/u-spr";
 const char g_mime_type[]= "text/x-pascal"; // TODO - create own MIME-type
 
-/* UNFINISHED
-class USpracheModel final : public QAbstractItemModel
+ProgramTreeNode ConvertProgramForOutline( const U::Synt::ProgramElements& elements )
 {
-	//Q_OBJECT
+	ProgramTreeNode result;
+	result.number_in_parent= 0;
+	result.name= QString( "global namespace" );
 
-public:
-	USpracheModel( QObject *parent = nullptr )
-		: QAbstractItemModel(parent)
+	for( const U::Synt::IProgramElementPtr& program_element : elements )
 	{
-		data_.push_back("@class foo");
-		data_.push_back("@class bar");
-		data_.push_back("@class baz");
-		data_.push_back("@function lol");
-	}
-
-	virtual QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const
-	{
-		if( column != 0 || row < 0 || row >= data_.size() )
-			return QModelIndex();
-
-		return createIndex( row, column );
-	}
-
-	virtual QModelIndex parent(const QModelIndex &child) const
-	{
-		if( child.row() == 0 && child.column() == 0 )
-			return QModelIndex();
-
-		return createIndex( 0, 0 );
-	}
-
-	virtual int rowCount(const QModelIndex &parent = QModelIndex()) const
-	{
-	}
-	virtual int columnCount(const QModelIndex &parent = QModelIndex()) const
-	{
-		return 1;
-	}
-
-	virtual QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const
-	{
-		if( index.row() >= data_.size() )
-			return QString("");
-
-		switch(role)
+		if( const auto namespace_= dynamic_cast<const U::Synt::Namespace*>( program_element.get() ) )
 		{
-		default:
-		case Qt::DisplayRole:
-		case Qt::EditRole:
-			return data_[ index.row() ];
-
-		case Qt::DecorationRole:
-			// TODO - return icon
-			return data_[ index.row() ];
-
-		};
-	}
-
-	Qt::ItemFlags flags(const QModelIndex &index) const
-	{
-		return Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsDragEnabled;
-	}
-
-	Qt::DropActions supportedDragActions() const
-	{
-		return Qt::MoveAction;
-	}
-
-	QStringList mimeTypes() const
-	{
-		//return Utils::DropSupport::mimeTypesForFilePaths();
-		return QStringList{ g_mime_type };
-	}
-
-	QMimeData *mimeData(const QModelIndexList &indexes) const
-	{
-		auto mime_data = new Utils::DropMimeData;
-		return mime_data;
-	}
-
-private:
-	QStringList data_;
-};
-*/
-
-class USpracheEditorWidget final : public TextEditor::TextEditorWidget
-{
-	//Q_OBJECT
-
-public:
-	USpracheEditorWidget()
-		: timer_(this)
-	{
-		timer_.setInterval(3000);
-		timer_.setSingleShot(true);
-	}
-
-private:
-	void finalizeInitialization() override
-	{
-		insertExtraToolBarWidget(TextEditorWidget::Left, new QLabel( QString("Ü editor"), this ) );
-
-		combo_box_.setMinimumContentsLength(20);
-
-		QSizePolicy policy = combo_box_.sizePolicy();
-		policy.setHorizontalPolicy(QSizePolicy::Expanding);
-		combo_box_.setSizePolicy(policy);
-		combo_box_.setMaxVisibleItems(40);
-
-		insertExtraToolBarWidget( TextEditorWidget::Left, &combo_box_ );
-
-		connect( this, &QPlainTextEdit::textChanged, this, &USpracheEditorWidget::OnTextChanged );
-		connect( &timer_, &QTimer::timeout, this, &USpracheEditorWidget::OnTimerExpired );
-
-		connect( &combo_box_, static_cast<void(QComboBox::*)(int)>(&QComboBox::activated), this, &USpracheEditorWidget::OnItemActivated );
-	}
-
-private:
-	void OnTextChanged()
-	{
-		//Core::MessageManager::write( QString( "text changed" ) );
-		timer_.stop();
-		timer_.start();
-	}
-
-	void OnTimerExpired()
-	{
-		// TODO - use custom model.
-
-		const U::ProgramString program_text= U::DecodeUTF8( USpracheEditorWidget::textDocument()->contents().toStdString() );
-
-		const U::LexicalAnalysisResult lex_result= U::LexicalAnalysis( program_text );
-		if( !lex_result.error_messages.empty() )
-			return;
-
-		U::Synt::SyntaxAnalysisResult synt_result= U::Synt::SyntaxAnalysis( lex_result.lexems );
-		if( !synt_result.error_messages.empty() )
-			return;
-
-		combo_box_.clear();
-		current_program_parsed_= std::move(synt_result.program_elements);
-
-		for( const U::Synt::IProgramElementPtr& program_element : current_program_parsed_ )
+			ProgramTreeNode element;
+			element.name= QString::fromUtf8( U::ToUTF8( namespace_->name_).data() );
+			element.childs= ConvertProgramForOutline( namespace_->elements_ ).childs;
+			element.number_in_parent= result.childs.size();
+			result.childs.push_back(element);
+		}
+		else if( const auto class_= dynamic_cast<const U::Synt::Class*>( program_element.get() ) )
 		{
-			if( const auto namespace_= dynamic_cast<const U::Synt::Namespace*>( program_element.get() ) )
-				combo_box_.addItem( QString::fromUtf8( U::ToUTF8( namespace_->name_).data() ) );
-			else if( const auto class_= dynamic_cast<const U::Synt::Class*>( program_element.get() ) )
-				combo_box_.addItem( QString::fromUtf8( U::ToUTF8( class_->name_ ).data() ) );
-			else if( const auto function_= dynamic_cast<const U::Synt::Function*>( program_element.get() ) )
-				combo_box_.addItem( QString::fromUtf8( U::ToUTF8( function_->name_.components.back().name ).data() ) );
-			else if( const auto variables_= dynamic_cast<const U::Synt::VariablesDeclaration*>( program_element.get() ) )
+			ProgramTreeNode element;
+			element.name= QString::fromUtf8( U::ToUTF8( class_->name_ ).data() );
+			element.number_in_parent= result.childs.size();
+			result.childs.push_back(element);
+		}
+		else if( const auto function_= dynamic_cast<const U::Synt::Function*>( program_element.get() ) )
+		{
+			ProgramTreeNode element;
+			element.name= QString::fromUtf8( U::ToUTF8( function_->name_.components.back().name ).data() );
+			element.number_in_parent= result.childs.size();
+			result.childs.push_back(element);
+		}
+		else if( const auto variables_= dynamic_cast<const U::Synt::VariablesDeclaration*>( program_element.get() ) )
+		{
+			for( const auto& variable : variables_->variables )
 			{
-				for( const auto& variable : variables_->variables )
-					combo_box_.addItem( QString::fromUtf8( U::ToUTF8( variable.name ).data() ) );
+				ProgramTreeNode element;
+				element.name= QString::fromUtf8( U::ToUTF8( variable.name ).data() );
+				element.number_in_parent= result.childs.size();
+				result.childs.push_back(element);
 			}
-			else if( const auto auto_variable_= dynamic_cast<const U::Synt::AutoVariableDeclaration*>( program_element.get() ) )
-				combo_box_.addItem( QString::fromUtf8( U::ToUTF8( auto_variable_->name ).data() ) );
+		}
+		else if( const auto auto_variable_= dynamic_cast<const U::Synt::AutoVariableDeclaration*>( program_element.get() ) )
+		{
+			ProgramTreeNode element;
+			element.name= QString::fromUtf8( U::ToUTF8( auto_variable_->name ).data() );
+			element.number_in_parent= result.childs.size();
+			result.childs.push_back(element);
 		}
 	}
 
-	void OnItemActivated( int index )
-	{
-		if( index < 0 || index >= static_cast<int>(current_program_parsed_.size()) )
-			return;
+	return result;
+}
 
-		const U::FilePos& file_pos= dynamic_cast<const U::Synt::SyntaxElementBase*>(current_program_parsed_[index].get())->file_pos_;
+///* UNFINISHED
 
-		Core::EditorManager::cutForwardNavigationHistory();
-		Core::EditorManager::addCurrentPositionToNavigationHistory();
-
-		setFocus();
-		gotoLine( file_pos.line, file_pos.pos_in_line );
-	}
-
-private:
-	QTimer timer_;
-	Utils::TreeViewComboBox combo_box_;
-	//USpracheModel combo_box_model_;
-
-	U::Synt::ProgramElements current_program_parsed_;
-};
-
-class USpracheEditorDocument final : public TextEditor::TextDocument
+USpracheEditorWidget::USpracheEditorWidget()
+	: timer_(this)
+	, combo_box_(this)
+	, combo_box_model_(this)
 {
-	//Q_OBJECT
+	timer_.setInterval(3000);
+	timer_.setSingleShot(true);
+}
 
-public:
-	USpracheEditorDocument()
-	{
-		setId(g_editor_id);
-
-		// setSyntaxHighlighter(new CppHighlighter);
-		// setIndenter(new CppTools::CppQtStyleIndenter);
-	}
-};
-
-class USpracheEditor final : public TextEditor::BaseTextEditor
+void USpracheEditorWidget::finalizeInitialization()
 {
-	//Q_OBJECT
+	insertExtraToolBarWidget(TextEditorWidget::Left, new QLabel( QString("Ü editor"), this ) );
 
-public:
-	USpracheEditor()
-	{
-	}
-};
+	combo_box_.setModel( &combo_box_model_ );
 
-class USpracheEditorFactory final : public TextEditor::TextEditorFactory
+	combo_box_.setMinimumContentsLength(20);
+
+	QSizePolicy policy = combo_box_.sizePolicy();
+	policy.setHorizontalPolicy(QSizePolicy::Expanding);
+	combo_box_.setSizePolicy(policy);
+	combo_box_.setMaxVisibleItems(40);
+
+	insertExtraToolBarWidget( TextEditorWidget::Left, &combo_box_ );
+
+	connect( this, &QPlainTextEdit::textChanged, this, &USpracheEditorWidget::OnTextChanged );
+	connect( &timer_, &QTimer::timeout, this, &USpracheEditorWidget::OnTimerExpired );
+
+	//connect( &combo_box_, static_cast<void(QComboBox::*)(int)>(&QComboBox::activated), this, &USpracheEditorWidget::OnItemActivated );
+}
+
+void USpracheEditorWidget::OnTextChanged()
 {
-public:
-	USpracheEditorFactory()
-	{
-		setId(g_editor_id);
-		setDisplayName(u8"Ü sprache editor");
-		addMimeType(g_mime_type);
+	//Core::MessageManager::write( QString( "text changed" ) );
+	timer_.stop();
+	timer_.start();
+}
 
-		setDocumentCreator([]() { return new USpracheEditorDocument; });
-		setEditorWidgetCreator([]() { return new USpracheEditorWidget; });
-		setEditorCreator([]() { return new USpracheEditor; });
-	}
+void USpracheEditorWidget::OnTimerExpired()
+{
+	Core::MessageManager::write( QString( "timer expired" ) );
 
-private:
-};
+	const U::ProgramString program_text= U::DecodeUTF8( USpracheEditorWidget::textDocument()->contents().toStdString() );
+
+	const U::LexicalAnalysisResult lex_result= U::LexicalAnalysis( program_text );
+	if( !lex_result.error_messages.empty() )
+		return;
+
+	U::Synt::SyntaxAnalysisResult synt_result= U::Synt::SyntaxAnalysis( lex_result.lexems );
+	if( !synt_result.error_messages.empty() )
+		return;
+
+	combo_box_model_.Update( ConvertProgramForOutline( synt_result.program_elements ) );
+	//combo_box_.view_->expandAll();
+}
+
+USpracheEditorDocument::USpracheEditorDocument()
+{
+	setId(g_editor_id);
+
+	// setSyntaxHighlighter(new CppHighlighter);
+	// setIndenter(new CppTools::CppQtStyleIndenter);
+}
+
+USpracheEditorFactory::USpracheEditorFactory()
+{
+	setId(g_editor_id);
+	setDisplayName(u8"Ü sprache editor");
+	addMimeType(g_mime_type);
+
+	setDocumentCreator([]() { return new USpracheEditorDocument; });
+	setEditorWidgetCreator([]() { return new USpracheEditorWidget; });
+	setEditorCreator([]() { return new USpracheEditor; });
+}
+
+void OnItemActivated( int index )
+{
+	/*
+	if( index < 0 || index >= static_cast<int>(current_program_parsed_->size()) )
+		return;
+
+	const U::FilePos& file_pos= dynamic_cast<const U::Synt::SyntaxElementBase*>((*current_program_parsed_)[index].get())->file_pos_;
+
+	Core::EditorManager::cutForwardNavigationHistory();
+	Core::EditorManager::addCurrentPositionToNavigationHistory();
+
+	setFocus();
+	gotoLine( file_pos.line, file_pos.pos_in_line );
+	*/
+}
+
 
 USprachePlugin::USprachePlugin()
 {
--- U_QtCreator_Plugin/uspracheplugin.h
+++ U_QtCreator_Plugin/uspracheplugin.h
@@ -2,10 +2,23 @@
 
 #include "usprache_global.h"
 
-#include <extensionsystem/iplugin.h>
+#include <QAbstractItemModel>
+#include <QComboBox>
+#include <QTimer>
+#include <QTreeView>
 
-namespace USprache {
-namespace Internal {
+#include <coreplugin/messagemanager.h>
+#include <extensionsystem/iplugin.h>
+#include <plugins/texteditor/textdocument.h>
+#include <plugins/texteditor/texteditor.h>
+#include <utils/dropsupport.h>
+
+
+namespace USprache
+{
+
+namespace Internal
+{
 
 class USprachePlugin : public ExtensionSystem::IPlugin
 {
@@ -23,6 +36,228 @@ public:
 private:
 	void triggerAction();
 };
+
+struct ProgramTreeNode
+{
+	QString name;
+	std::vector<ProgramTreeNode> childs;
+	ProgramTreeNode* parent= nullptr;
+	size_t number_in_parent= 0;
+	int line= 0, pos_in_line= 0;
+};
+
+class USpracheModel final : public QAbstractItemModel
+{
+	Q_OBJECT
+
+public:
+	enum Role {
+		FileNameRole = Qt::UserRole + 1,
+		LineNumberRole
+	};
+
+	USpracheModel( QObject *parent = nullptr )
+		: QAbstractItemModel(parent)
+	{
+		tree_.name= QString( "<no data>" );
+	}
+
+	~USpracheModel(){}
+
+	void Update( ProgramTreeNode tree )
+	{
+		beginResetModel();
+		tree_= std::move(tree);
+		FixParents( tree_ );
+		endResetModel();
+	}
+
+	virtual QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const override
+	{
+		Q_UNUSED(column);
+		if( !parent.isValid() )
+			return createIndex( row, 0, const_cast<ProgramTreeNode*>(&tree_.childs[row]) );
+		else
+		{
+			const ProgramTreeNode* const parent_ptr= reinterpret_cast<const ProgramTreeNode*>(parent.internalPointer());
+			return createIndex( row, 0, const_cast<ProgramTreeNode*>(&parent_ptr->childs[row]) );
+		}
+	}
+
+	virtual QModelIndex parent(const QModelIndex& element) const override
+	{
+		if( !element.isValid() )
+			return QModelIndex();
+
+		const ProgramTreeNode* const element_ptr= reinterpret_cast<const ProgramTreeNode*>(element.internalPointer());
+		if( element_ptr->parent == nullptr )
+			return QModelIndex();
+
+		return createIndex( element_ptr->parent->number_in_parent, 0, const_cast<ProgramTreeNode*>(element_ptr->parent) ); // TODO - select correct row
+	}
+
+	virtual int rowCount(const QModelIndex &parent = QModelIndex()) const override
+	{
+		if( !parent.isValid() )
+			return tree_.childs.size();
+
+		const ProgramTreeNode* const element_ptr= reinterpret_cast<const ProgramTreeNode*>(parent.internalPointer());
+		return int(element_ptr->childs.size());
+
+	}
+	virtual int columnCount(const QModelIndex &parent = QModelIndex()) const override
+	{
+		Q_UNUSED(parent);
+		return 1;
+	}
+
+	virtual QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override
+	{
+		const ProgramTreeNode* const ptr= reinterpret_cast<const ProgramTreeNode*>(index.internalPointer());
+		if( ptr == nullptr )
+			return QVariant();
+
+		switch(role)
+		{
+		case Qt::DisplayRole:
+			return ptr->name;
+
+		case Qt::DecorationRole:
+			// TODO - return icon
+			//return QString( "info.ico" );
+
+		case FileNameRole:
+			return QString("some_file");
+
+		case LineNumberRole:
+			return QString("666");
+
+		case Qt::EditRole:
+		default:
+			break;
+		};
+		return QVariant();
+	}
+
+	Qt::ItemFlags flags(const QModelIndex &index) const override
+	{
+		if (!index.isValid())
+			return 0;
+
+		return Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsDragEnabled;
+	}
+
+	Qt::DropActions supportedDragActions() const override
+	{
+		return Qt::MoveAction;
+	}
+
+	QStringList mimeTypes() const override
+	{
+		return Utils::DropSupport::mimeTypesForFilePaths();
+	}
+
+private:
+	void FixParents( ProgramTreeNode& tree )
+	{
+		for( ProgramTreeNode& child : tree.childs )
+		{
+			child.parent= &tree;
+			FixParents(child);
+		}
+	}
+
+private:
+	ProgramTreeNode tree_;
+};
+
+class TreeViewComboBox : public QComboBox
+{
+	Q_OBJECT
+
+public:
+	TreeViewComboBox(QWidget *parent = 0)
+		: QComboBox(parent)
+	{
+		view_ = new QTreeView;
+		view_->setHeaderHidden(true);
+		view_->setItemsExpandable(true);
+		view_->expandAll();
+		setView(view_);
+
+		 view_->viewport()->installEventFilter(this);
+	}
+
+	bool eventFilter(QObject *object, QEvent *event)
+	{
+		if (event->type() == QEvent::MouseButtonPress && object == view()->viewport()) {
+			QMouseEvent* mouseEvent = static_cast<QMouseEvent*>(event);
+			QModelIndex index = view()->indexAt(mouseEvent->pos());
+			if (!view()->visualRect(index).contains(mouseEvent->pos()))
+				skip_next_hide_ = true;
+		}
+		return false;
+	}
+
+	void hidePopup()
+	{
+		if (skip_next_hide_)
+			skip_next_hide_ = false;
+		else
+			QComboBox::hidePopup();
+	}
+
+private:
+	QTreeView *view_;
+	bool skip_next_hide_= false;
+};
+
+class USpracheEditorWidget final : public TextEditor::TextEditorWidget
+{
+	Q_OBJECT
+
+public:
+	USpracheEditorWidget();
+
+private:
+	void finalizeInitialization() override;
+
+private:
+	void OnTextChanged();
+	void OnTimerExpired();
+	void OnItemActivated( int index );
+
+private:
+	QTimer timer_;
+	TreeViewComboBox combo_box_;
+	USpracheModel combo_box_model_;
+};
+
+class USpracheEditorDocument final : public TextEditor::TextDocument
+{
+	Q_OBJECT
+
+public:
+	USpracheEditorDocument();
+};
+
+class USpracheEditor final : public TextEditor::BaseTextEditor
+{
+	Q_OBJECT
+
+public:
+	USpracheEditor()
+	{
+	}
+};
+
+class USpracheEditorFactory final : public TextEditor::TextEditorFactory
+{
+public:
+	USpracheEditorFactory();
+private:
+};
+
 
 } // namespace Internal
 } // namespace USprache
