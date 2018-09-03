9c5beeb6f11dc6a5b7eb8d23f1bda478a3da3f46
Panzerschrek <AntiPanzerschrek@yandex.ru>
2018-09-03 10:02:09 +0700

    Ispravlenije padenija.

--- U_QtCreator_Plugin/program_model.cpp
+++ U_QtCreator_Plugin/program_model.cpp
@@ -404,7 +404,7 @@ static std::vector<ProgramModel::ProgramTreeNode> BuildProgramModel_r( const Syn
 			element.name= ProgramStringToQString( typedef_->name );
 			element.kind= ProgramModel::ElementKind::Typedef;
 			element.number_in_parent= result.size();
-			element.file_pos= class_->file_pos_;
+			element.file_pos= typedef_->file_pos_;
 			result.push_back(element);
 		}
 		else if( const auto function_= dynamic_cast<const Synt::Function*>( class_element.get() ) )
