97907a9b5a9dbcc2bcc618b0b9fed7185953fbb7
Panzerschrek <AntiPanzerschrek@yandex.ru>
2018-09-03 15:13:28 +0700

    Realizovana podsvetka raznogo vida skobok.

--- QtCreator_Plugin/syntax_highlighter.cpp
+++ QtCreator_Plugin/syntax_highlighter.cpp
@@ -1,5 +1,7 @@
 #include <texteditor/texteditorconstants.h>
+#include <texteditor/textdocumentlayout.h>
 
+#include "../Compiler/src/assert.hpp"
 #include "../Compiler/src/lexical_analyzer.hpp"
 #include "../Compiler/src/keywords.hpp"
 #include "strings.h"
@@ -11,6 +13,22 @@ namespace U
 
 namespace QtCreatorPlugin
 {
+
+static QChar ParenthesCodeForLexem( const Lexem::Type lexem_type )
+{
+	switch( lexem_type )
+	{
+	case Lexem::Type::BracketLeft : return '(';
+	case Lexem::Type::BracketRight: return ')';
+	case Lexem::Type::SquareBracketLeft : return '[';
+	case Lexem::Type::SquareBracketRight: return ']';
+	case Lexem::Type::BraceLeft : return '{';
+	case Lexem::Type::BraceRight: return '}';
+	case Lexem::Type::TemplateBracketLeft : return '<';
+	case Lexem::Type::TemplateBracketRight: return '>';
+	default: U_ASSERT(false); return ' ';
+	}
+}
 
 SyntaxHighlighter::SyntaxHighlighter()
 {
@@ -36,9 +54,13 @@ void SyntaxHighlighter::highlightBlock( const QString& text )
 		return;
 	}
 
+	QVector<TextEditor::Parenthesis> parentheses;
+
 	setFormat( 0, text.size(), formatForCategory( int(Formats::Whitespace) ) );
 	for( size_t i= 0u; i < lex_result.lexems.size(); ++i )
 	{
+		const Lexem& lexem= lex_result.lexems[i];
+
 		FilePos next_file_pos;
 		if( i + 1u < lex_result.lexems.size() )
 		{
@@ -49,15 +71,14 @@ void SyntaxHighlighter::highlightBlock( const QString& text )
 			next_file_pos.file_index= 0;
 			next_file_pos.line= next_file_pos.pos_in_line= text.size();
 		}
+		// TODO - what if we have more, then one line?
+		const int current_linear_pos= lexem.file_pos.pos_in_line;
+		const int next_linear_pos= next_file_pos.pos_in_line;
 
-		const Lexem& lexem= lex_result.lexems[i];
-
+		// Setup highlighting.
 		Formats format;
 		switch( lexem.type )
 		{
-		case Lexem::Type::None:
-			format= Formats::Regular; break;
-
 		case Lexem::Type::Comment:
 			format= Formats::Comment; break;
 
@@ -74,70 +95,41 @@ void SyntaxHighlighter::highlightBlock( const QString& text )
 		case Lexem::Type::Number:
 			format= Formats::Number; break;
 
-		case Lexem::Type::LiteralSuffix:
-		case Lexem::Type::BracketLeft:
-		case Lexem::Type::BracketRight:
-		case Lexem::Type::SquareBracketLeft:
-		case Lexem::Type::SquareBracketRight:
-		case Lexem::Type::BraceLeft:
-		case Lexem::Type::BraceRight:
-		case Lexem::Type::TemplateBracketLeft :
-		case Lexem::Type::TemplateBracketRight:
-		case Lexem::Type::Scope:
-		case Lexem::Type::Comma:
-		case Lexem::Type::Dot:
-		case Lexem::Type::Colon:
-		case Lexem::Type::Semicolon:
-		case Lexem::Type::Assignment:
-		case Lexem::Type::Plus:
-		case Lexem::Type::Minus:
-		case Lexem::Type::Star:
-		case Lexem::Type::Slash:
-		case Lexem::Type::Percent:
-		case Lexem::Type::And:
-		case Lexem::Type::Or:
-		case Lexem::Type::Xor:
-		case Lexem::Type::Tilda:
-		case Lexem::Type::Not:
-		case Lexem::Type::Apostrophe:
-		case Lexem::Type::Increment:
-		case Lexem::Type::Decrement:
-		case Lexem::Type::CompareLess:
-		case Lexem::Type::CompareGreater:
-		case Lexem::Type::CompareEqual:
-		case Lexem::Type::CompareNotEqual:
-		case Lexem::Type::CompareLessOrEqual:
-		case Lexem::Type::CompareGreaterOrEqual:
-		case Lexem::Type::Conjunction:
-		case Lexem::Type::Disjunction:
-		case Lexem::Type::AssignAdd:
-		case Lexem::Type::AssignSub:
-		case Lexem::Type::AssignMul:
-		case Lexem::Type::AssignDiv:
-		case Lexem::Type::AssignRem:
-		case Lexem::Type::AssignAnd:
-		case Lexem::Type::AssignOr :
-		case Lexem::Type::AssignXor:
-		case Lexem::Type::ShiftLeft :
-		case Lexem::Type::ShiftRight:
-		case Lexem::Type::AssignShiftLeft :
-		case Lexem::Type::AssignShiftRight:
-		case Lexem::Type::LeftArrow:
-		case Lexem::Type::Ellipsis:
-		case Lexem::Type::EndOfFile:
+		case Lexem::Type::None:
+		default:
 			format= Formats::Regular; break;
 		}
 
-		// TODO - what if we have more, then one line?
-		setFormat( lexem.file_pos.pos_in_line, next_file_pos.pos_in_line, formatForCategory( int(format) ) );
 
-		int whitespace_start= next_file_pos.pos_in_line - 1;
+		setFormat( current_linear_pos, next_linear_pos, formatForCategory( int(format) ) );
+
+		int whitespace_start= next_linear_pos - 1;
 		while( whitespace_start >= 0 && text[whitespace_start].isSpace() )
 			--whitespace_start;
 		++whitespace_start;
 
-		setFormat( whitespace_start, next_file_pos.pos_in_line, formatForCategory( int(Formats::Whitespace) ) );
+		setFormat( whitespace_start, next_linear_pos, formatForCategory( int(Formats::Whitespace) ) );
+
+		// Setup parentheses.
+		switch( lexem.type )
+		{
+		case Lexem::Type::BracketLeft:
+		case Lexem::Type::SquareBracketLeft:
+		case Lexem::Type::BraceLeft:
+		case Lexem::Type::TemplateBracketLeft :
+			parentheses.push_back( TextEditor::Parenthesis( TextEditor::Parenthesis::Opened, ParenthesCodeForLexem(lexem.type), current_linear_pos ) );
+			break;
+
+		case Lexem::Type::BracketRight:
+		case Lexem::Type::SquareBracketRight:
+		case Lexem::Type::BraceRight:
+		case Lexem::Type::TemplateBracketRight:
+			parentheses.push_back( TextEditor::Parenthesis( TextEditor::Parenthesis::Closed, ParenthesCodeForLexem(lexem.type), current_linear_pos ) );
+			break;
+		}
 	} // for lexems
+
+	TextEditor::TextDocumentLayout::setParentheses(currentBlock(), parentheses);
 }
 
 } // namespace QtCreatorPlugin
