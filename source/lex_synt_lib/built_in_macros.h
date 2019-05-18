0055a6d3dd081da1c140d54aa75e1c715e8bb4eb
Panzerschrek <AntiPanzerschrek@yandex.ru>
2019-05-18 10:11:42 +0700

    Dobavlen makros "if_var" dlä razvoracivanija opçionaljnyh znacenij.

--- source/lex_synt_lib/built_in_macros.h
+++ source/lex_synt_lib/built_in_macros.h
@@ -1,4 +1,14 @@
 R"(
-
-
+?macro <? if_var:block ( ?r:opt<? & ?> ?m:opt<? mut ?> ?var_name:ident : ?e:expr ) ?b:block ?>
+->
+<?
+	{
+		auto lock_temps& ?m<? mut ?> e_result= ?e;
+		if( !e_result.empty() )
+		{
+			auto lock_temps ?r<? & ?> ?m<? mut ?> ?var_name= e_result.get_ref();
+			?b
+		}
+	}
+?>
 )"
