00368f2186f9e012816eb902bd8c245627de1770
Panzerschrek <AntiPanzerschrek@yandex.ru>
2019-06-15 14:59:25 +0700

    Realizovan makros "foreach".

--- source/lex_synt_lib/built_in_macros.h
+++ source/lex_synt_lib/built_in_macros.h
@@ -1,4 +1,5 @@
 R"(
+
 ?macro <? if_var:block ( ?r:opt<? & ?> ?m:opt<? mut ?> ?var_name:ident : ?e:expr ) ?b:block ?>
 ->
 <?
@@ -11,4 +12,23 @@ R"(
 		}
 	}
 ?>
+
+?macro <? foreach:block ( ?r:opt<? & ?> ?m:opt<? mut ?> ?var_name:ident : ?e:expr ) ?b:block ?>
+->
+<?
+	{
+		auto lock_temps& ?m<? mut ?> e_result= ?e;
+		auto mut r= e_result.range();
+		while( !r.empty() )
+		{
+			unsafe
+			{
+				auto lock_temps ?r<? & ?> ?m<? mut ?> ?var_name= r[size_type(0)];
+				safe{ ?b }
+				r.pop_front();
+			}
+		}
+	}
+?>
+
 )"
