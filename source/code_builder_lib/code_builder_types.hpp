608d8de67850b4d029de7f555326d418fd69c9ae
Panzerschrek <AntiPanzerschrek@yandex.ru>
2019-07-31 16:42:20 +0700

    V "typeinfo" dlä klassov i struktur dobavleno smescenije tabliçy virtualjnyh funkçij.

--- source/code_builder_lib/code_builder_types.hpp
+++ source/code_builder_lib/code_builder_types.hpp
@@ -660,14 +660,14 @@ public:
 
 	Kind kind= Kind::Struct;
 	ClassProxyPtr base_class; // We can have single non-interface base class.
-	unsigned int base_class_field_number= 0u;
+	unsigned int base_class_field_number= ~0u;
 	std::vector<ClassProxyPtr> parents; // Class have fields with numbers 0-N for parents.
 	std::vector<unsigned int> parents_fields_numbers;
 
 	std::vector<VirtualTableEntry> virtual_table;
 	llvm::StructType* virtual_table_llvm_type= nullptr;
 	llvm::GlobalVariable* this_class_virtual_table= nullptr; // May be null for interfaces and abstract classes.
-	unsigned int virtual_table_field_number= 0u;
+	unsigned int virtual_table_field_number= ~0u;
 
 	// Key - sequence of classes from child to parent. This class not included.
 	// Virtual table destination is lats key element.
