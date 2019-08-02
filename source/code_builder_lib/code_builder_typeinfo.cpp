cbb2ec2f119d51b81319dd5b80a3c15582a76405
Panzerschrek <AntiPanzerschrek@yandex.ru>
2019-08-02 17:26:25 +0700

    Udaleno boleje ne nužnoje v "typeinfo" pole.

--- source/code_builder_lib/code_builder_typeinfo.cpp
+++ source/code_builder_lib/code_builder_typeinfo.cpp
@@ -212,8 +212,6 @@ void CodeBuilder::BuildFullTypeinfo( const Type& type, Variable& typeinfo_variab
 		add_bool_field( "is_typeinfo"_SpC, class_type->is_typeinfo );
 		add_bool_field( "shared"_SpC, class_type->have_shared_state );
 
-		add_size_field( "virtual_table_offset"_SpC, 0u ); // Virtual table offset is always zero.
-
 		const ClassProxyPtr class_proxy= type.GetClassTypeProxy();
 		add_list_head_field( "fields_list"_SpC   , BuildTypeinfoClassFieldsList(    class_proxy, root_namespace ) );
 		add_list_head_field( "types_list"_SpC    , BuildTypeinfoClassTypesList(     class_proxy, root_namespace ) );
