c07d8358de84bcc8c1e411e0a0ba08c2c1d457a1
Panzerschrek <AntiPanzerschrek@yandex.ru>
2019-08-04 11:32:38 +0700

    Dobavlen proekt preobrazovatelä zagolovkov.

---
+++ source/cpp_header_converter/main.cpp
@@ -0,0 +1,8 @@
+#include "../code_builder_lib/push_disable_llvm_warnings.hpp"
+#include <clang/Tooling/Tooling.h>
+#include "../code_builder_lib/pop_llvm_warnings.hpp"
+
+int main()
+{
+	return 0;
+}
