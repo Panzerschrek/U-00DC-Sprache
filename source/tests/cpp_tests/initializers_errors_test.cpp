7ac653868944bc1101a6c5dfaa83839b2a11275b
Panzerschrek <AntiPanzerschrek@yandex.ru>
2019-08-16 12:02:14 +0700

    Test na popytku kopirovanija nekopirujemogo korteža cerez iniçializaçiju cerez =.

--- source/tests/cpp_tests/initializers_errors_test.cpp
+++ source/tests/cpp_tests/initializers_errors_test.cpp
@@ -700,4 +700,28 @@ U_TEST(TuplesInitializersErrors_Test7)
 	U_TEST_ASSERT( error.file_pos.line == 4u );
 }
 
+U_TEST(TuplesInitializersErrors_Test8)
+{
+	static const char c_program_text[]=
+	R"(
+		struct S
+		{
+			fn constructor( mut this, S &imut other )= delete;
+		}
+		fn Foo()
+		{
+			var tup( S ) t0= zero_init;
+			var tup( S ) t1= t0;
+		}
+	)";
+
+	const ICodeBuilder::BuildResult build_result= BuildProgramWithErrors( c_program_text );
+
+	U_TEST_ASSERT( !build_result.errors.empty() );
+	const CodeBuilderError& error= build_result.errors.front();
+
+	U_TEST_ASSERT( error.code == CodeBuilderErrorCode::OperationNotSupportedForThisType );
+	U_TEST_ASSERT( error.file_pos.line == 9u );
+}
+
 } // namespace U
