31ff0adf39e6f0e19fb213cfd6162e6540727649
Panzerschrek <AntiPanzerschrek@yandex.ru>
2019-11-13 09:31:43 +0300

    Zadavleny nekotoryje predupreždenija iz "llvm" pod "MSVC".

--- source/code_builder_lib/push_disable_llvm_warnings.hpp
+++ source/code_builder_lib/push_disable_llvm_warnings.hpp
@@ -13,8 +13,12 @@
 
 #pragma warning( push )
 
-#pragma warning( disable : 4624 )
+#pragma warning( disable : 4141 )
+#pragma warning( disable : 4146 )
 #pragma warning( disable : 4244 )
 #pragma warning( disable : 4267 )
+#pragma warning( disable : 4291 )
+#pragma warning( disable : 4309 )
+#pragma warning( disable : 4624 )
 
 #endif
