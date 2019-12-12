1b29419d3fc91a6d5e7db1611fab93cdf0d78727
Panzerschrek <AntiPanzerschrek@yandex.ru>
2019-12-12 14:36:25 +0300

    Uproscenije raboty s klücevymi slovami.

--- source/lex_synt_lib/keywords.cpp
+++ source/lex_synt_lib/keywords.cpp
@@ -5,53 +5,32 @@
 namespace U
 {
 
-namespace
-{
-
-struct Globals
-{
-	const std::string (&keywords)[ size_t(Keywords::LastKeyword) ];
-	const ProgramStringSet& keywords_set;
-};
-
 // Hack for initialization.
 // Use in-function static variables for cross-module initialization order setup.
-const Globals& GetGlobals()
+
+bool IsKeyword( const std::string& str )
 {
+	static const ProgramStringSet c_keywords_set
+	{
+		#define PROCESS_KEYWORD(x) #x,
+		#include "keywords_list.hpp"
+		#undef PROCESS_KEYWORD
+	};
+
+	return c_keywords_set.count( str ) != 0;
+}
+
+const std::string& Keyword( Keywords keyword )
+{
+	U_ASSERT( keyword < Keywords::LastKeyword );
 	static const std::string c_keywords[ size_t(Keywords::LastKeyword) ]
 	{
 		#define PROCESS_KEYWORD(x) #x,
 		#include "keywords_list.hpp"
 		#undef PROCESS_KEYWORD
 	};
-	
-	static const ProgramStringSet c_keywords_set=
-	[]() -> ProgramStringSet
-	{
-		ProgramStringSet result;
-		for( const std::string& k : c_keywords )
-			result.emplace( k );
-		return result;
-	}
-	();
 
-	static const Globals c_globals{ c_keywords, c_keywords_set };
-
-	return c_globals;
-}
-
-} // namespace
-
-bool IsKeyword( const std::string& str )
-{
-	return GetGlobals().keywords_set.count( str ) != 0;
-}
-
-const std::string& Keyword( Keywords keyword )
-{
-	U_ASSERT( keyword < Keywords::LastKeyword );
-
-	return GetGlobals().keywords[ size_t(keyword) ];
+	return c_keywords[ size_t(keyword) ];
 }
 
 bool operator==( Keywords keyword, const std::string& str )
