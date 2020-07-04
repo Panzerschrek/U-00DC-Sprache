3a0c6bc99dd97e68635b62c97dcb3e2119818b6d
Panzerschrek <AntiPanzerschrek@yandex.ru>
2020-07-04 17:39:42 +0300

    Proveräjem tip pri prisvaivanii.

--- source/compiler1/tests/cpp_tests_launcher.cpp
+++ source/compiler1/tests/cpp_tests_launcher.cpp
@@ -37,6 +37,7 @@ bool FilterTest( const std::string& test_name )
 		"Redefinition5",
 		"SimpliestProgramTest",
 		"SimpleProgramTest",
+		"TypesMismatchTest2",
 		"TypesMismatchTest3",
 
 		"auto_variables_test.cpp:AutoVariableTest0",
