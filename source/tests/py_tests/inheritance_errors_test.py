38963de2d6e2a027e8115218a6b74b8b13df6541
Panzerschrek <AntiPanzerschrek@yandex.ru>
2020-08-24 12:08:28 +0300

    Poroždajem ošibku pri popytke skonstruirovatj abstraktnyj klass ili interfejs.

--- source/tests/py_tests/inheritance_errors_test.py
+++ source/tests/py_tests/inheritance_errors_test.py
@@ -262,9 +262,7 @@ def ConstructingAbstractClassOrInterface_Test1():
 		}
 	"""
 	errors_list= ConvertErrors( tests_lib.build_program_with_errors( c_program_text ) )
-	assert( len(errors_list) > 1 )
-	assert( errors_list[1].error_code == "ConstructingAbstractClassOrInterface" )
-	assert( errors_list[1].file_pos.line == 6 )
+	assert( HaveError( errors_list, "ConstructingAbstractClassOrInterface", 6 ) )
 
 
 def ConstructingAbstractClassOrInterface_Test2():
@@ -277,9 +275,7 @@ def ConstructingAbstractClassOrInterface_Test2():
 		}
 	"""
 	errors_list= ConvertErrors( tests_lib.build_program_with_errors( c_program_text ) )
-	assert( len(errors_list) > 0 )
-	assert( errors_list[0].error_code == "ConstructingAbstractClassOrInterface" )
-	assert( errors_list[0].file_pos.line == 6 )
+	assert( HaveError( errors_list, "ConstructingAbstractClassOrInterface", 6 ) )
 
 
 def ConstructingAbstractClassOrInterface_Test3():
