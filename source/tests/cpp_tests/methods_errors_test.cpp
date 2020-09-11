3b9bda8f152a48cad243c2dbacca6f73faaa60dc
Panzerschrek <AntiPanzerschrek@yandex.ru>
2020-09-11 14:11:41 +0300

    Uproscenije pravil filjtraçii testov.

--- source/tests/cpp_tests/methods_errors_test.cpp
+++ source/tests/cpp_tests/methods_errors_test.cpp
@@ -76,10 +76,7 @@ U_TEST( ClassFiledAccessInStaticMethodTest0 )
 
 	const ErrorTestBuildResult build_result= BuildProgramWithErrors( c_program_text );
 
-	U_TEST_ASSERT( !build_result.errors.empty() );
-	const CodeBuilderError& error= build_result.errors.front();
-	U_TEST_ASSERT( error.code == CodeBuilderErrorCode::ClassFiledAccessInStaticMethod );
-	U_TEST_ASSERT( error.file_pos.GetLine() == 7u );
+	U_TEST_ASSERT( HaveError( build_result.errors, CodeBuilderErrorCode::ClassFiledAccessInStaticMethod, 7u ) );
 }
 
 U_TEST(FunctionBodyDuplication_ForMethods_Test0)
