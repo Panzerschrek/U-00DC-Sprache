e56e1a5a5e13a91994992279450a36a92cd5681f
Panzerschrek <AntiPanzerschrek@yandex.ru>
2020-10-18 12:39:24 +0300

    Realizovano postrojenije "type_id" v Kompilätore1. Dopolnen test na "type_id": teperj proveräjetsä takže ravenstvo "type_id" v "typeinfo" i v tabliçe virtualjnyh funkçij.

--- source/tests/type_id_test/entry.cpp
+++ source/tests/type_id_test/entry.cpp
@@ -3,10 +3,13 @@
 
 extern "C" void* GetSomeClassIdA();
 extern "C" void* GetSomeClassIdB();
+extern "C" void* GetSomeClassIdTypeinfo();
 
 int main()
 {
 	std::cout << "A id: " << GetSomeClassIdA() << std::endl;
-	std::cout << "A id: " << GetSomeClassIdB() << std::endl;
+	std::cout << "B id: " << GetSomeClassIdB() << std::endl;
+	std::cout << "Typeinfo id: " << GetSomeClassIdTypeinfo() << std::endl;
 	assert( GetSomeClassIdA() == GetSomeClassIdB() );
+	assert( GetSomeClassIdA() == GetSomeClassIdTypeinfo() );
 }
