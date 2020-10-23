bb5cca983ef265e0af0489c193e750b693e0259a
Panzerschrek <AntiPanzerschrek@yandex.ru>
2020-10-23 11:13:22 +0300

    Sobirajem na sborocnom servere Kompilätor2.

--- ci_build.sh
+++ ci_build.sh
@@ -8,11 +8,12 @@ tar -xf clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04.tar &&\
 # Configure build
 mkdir build-travis &&\
 cd build-travis &&\
-cmake ../source/ -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_LIB_DIR=../clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/lib/ -DSPHINX_WARNINGS_AS_ERRORS=No &&\
+cmake ../source/ -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_LIB_DIR=../clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04/lib/ -DU_BUILD_COMPILER2=YES -DSPHINX_WARNINGS_AS_ERRORS=No &&\
 \
 cmake --build . &&\
 \
 # Run ustlib tests
 cd .. &&\
 python3 source/annotated_tests_run.py --compiler-executable build-travis/compiler0/Compiler  --use-position-independent-code --input-dir source/ustlib_test &&\
-python3 source/annotated_tests_run.py --compiler-executable build-travis/compiler1/Compiler1 --use-position-independent-code --input-dir source/ustlib_test
+python3 source/annotated_tests_run.py --compiler-executable build-travis/compiler1/Compiler1 --use-position-independent-code --input-dir source/ustlib_test &&\
+python3 source/annotated_tests_run.py --compiler-executable build-travis/compiler2/Compiler2 --use-position-independent-code --input-dir source/ustlib_test
