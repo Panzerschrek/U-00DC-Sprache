ca86d7c95afede8baa7e7f80606fbe91ad17ffac
Panzerschrek <AntiPanzerschrek@yandex.ru>
2022-01-03 18:38:32 +0300

    Obnovlön god v "Copyright".

--- source/docs/ru/conf.py
+++ source/docs/ru/conf.py
@@ -6,7 +6,7 @@ from sphinx.highlighting import lexers
 
 project = "Ü Sprache"
 author = "Panzerschrek"
-copyright = "Panzerschrek 2019-2021"
+copyright = "Panzerschrek 2019-2022"
 html_theme = "sphinxdoc"
 language = "ru"
 html_title = "Ü документация"
