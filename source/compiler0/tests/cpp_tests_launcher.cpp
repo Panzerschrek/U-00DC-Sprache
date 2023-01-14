8735c2c3fd1e96303bac20315800d21e85f4cdb0
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-01-14 21:57:22 +0300

    Zakostylen odin iz testov, cto padajet iz-za glükov interpretatora biblioteki "LLVM".

--- source/compiler0/tests/cpp_tests_launcher.cpp
+++ source/compiler0/tests/cpp_tests_launcher.cpp
@@ -94,7 +94,7 @@ static CodeBuilderOptions GetCodeBuilderOptionsForTests()
 	CodeBuilderOptions options;
 	options.build_debug_info= true;
 	options.generate_tbaa_metadata= true;
-	options.create_lifetimes= false; // TODO - fix this. lifetimes intrisincs calls are broken in LLVM 15.
+	options.create_lifetimes= true;
 	return options;
 }
 
