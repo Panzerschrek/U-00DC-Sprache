91904c1066edc185eb1208465a36856fa1379de4
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-01-16 11:50:00 +0300

    Popravleno vystavlenije "dereferenceable" atributa v Kompilätore1.

--- source/compiler1/code_builder_lib/llvm_ext.cpp
+++ source/compiler1/code_builder_lib/llvm_ext.cpp
@@ -24,7 +24,15 @@ extern "C" bool U1_BasicBlockHasPredecessors(const LLVMBasicBlockRef basic_block
 
 extern "C" void U1_FunctionAddDereferenceableAttr(const LLVMValueRef function, const uint32_t index, const uint64_t bytes)
 {
-	llvm::dyn_cast<llvm::Function>(llvm::unwrap(function))->addDereferenceableAttr(index, bytes);
+	const auto f= llvm::dyn_cast<llvm::Function>(llvm::unwrap(function));
+
+	llvm::AttrBuilder builder( f->getContext() );
+	builder.addDereferenceableAttr( bytes );
+
+	if( index == llvm::AttributeList::ReturnIndex )
+		f->addRetAttrs(builder);
+	else
+		f->addParamAttrs(index - llvm::AttributeList::FirstArgIndex, builder);
 }
 
 extern "C" size_t U1_ConvertUTF8ToUTF16(
