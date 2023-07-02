0ee5af064afd1b91a0edd36328869c32fae38920
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-07-02 16:37:12 +0300

    Ispravlena ošibka v Kompilätore1 - ne poroždalosj soobscenije ob ispoljzovanii klücevogo slova v kacestve imeni prostranstva imön.

--- source/tests/cpp_tests/namespaces_errors_test.cpp
+++ source/tests/cpp_tests/namespaces_errors_test.cpp
@@ -81,6 +81,29 @@ U_TEST( Redefenition_ForNamespaces_Test0 )
 	U_TEST_ASSERT( error.src_loc.GetLine() == 3u );
 }
 
+U_TEST( UsingKeywordAsName_ForNamespaces_Test0 )
+{
+	static const char c_program_text[]=
+	R"(
+		namespace struct {}
+		namespace this {}
+		namespace public {}
+		namespace virtual {}
+		namespace break {}
+		namespace pure {}
+	)";
+
+	const ErrorTestBuildResult build_result= BuildProgramWithErrors( c_program_text );
+
+	U_TEST_ASSERT( build_result.errors.size() >= 6 );
+	U_TEST_ASSERT( HaveError( build_result.errors, CodeBuilderErrorCode::UsingKeywordAsName, 2 ) );
+	U_TEST_ASSERT( HaveError( build_result.errors, CodeBuilderErrorCode::UsingKeywordAsName, 3 ) );
+	U_TEST_ASSERT( HaveError( build_result.errors, CodeBuilderErrorCode::UsingKeywordAsName, 4 ) );
+	U_TEST_ASSERT( HaveError( build_result.errors, CodeBuilderErrorCode::UsingKeywordAsName, 5 ) );
+	U_TEST_ASSERT( HaveError( build_result.errors, CodeBuilderErrorCode::UsingKeywordAsName, 6 ) );
+	U_TEST_ASSERT( HaveError( build_result.errors, CodeBuilderErrorCode::UsingKeywordAsName, 7 ) );
+}
+
 } // namespace
 
 } // namespace U
