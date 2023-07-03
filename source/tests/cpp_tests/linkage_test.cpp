09763d83d6d8d572d1f18796efe19e7443642ade
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-07-03 19:19:37 +0300

    Udostoveräjemsä v testah v nalicii otsutstvija "comdat".

--- source/tests/cpp_tests/linkage_test.cpp
+++ source/tests/cpp_tests/linkage_test.cpp
@@ -33,18 +33,22 @@ U_TEST( FunctionLinkage_Test0 )
 	const llvm::Function* const foo= module->getFunction( "_Z3Foov" );
 	U_TEST_ASSERT( foo != nullptr );
 	U_TEST_ASSERT( foo->getLinkage() == llvm::GlobalValue::PrivateLinkage );
+	U_TEST_ASSERT( !foo->hasComdat() );
 
 	const llvm::Function* const baz= module->getFunction( "_ZN2NN3BazEv" );
 	U_TEST_ASSERT( baz != nullptr );
 	U_TEST_ASSERT( baz->getLinkage() == llvm::GlobalValue::PrivateLinkage );
+	U_TEST_ASSERT( !baz->hasComdat() );
 
 	const llvm::Function* const bar= module->getFunction( "_ZN1S3BarERKS_" );
 	U_TEST_ASSERT( bar != nullptr );
 	U_TEST_ASSERT( bar->getLinkage() == llvm::GlobalValue::PrivateLinkage );
+	U_TEST_ASSERT( !bar->hasComdat() );
 
 	const llvm::Function* const static_method= module->getFunction( "_ZN1S12StaticMethodEv" );
 	U_TEST_ASSERT( static_method != nullptr );
 	U_TEST_ASSERT( static_method->getLinkage() == llvm::GlobalValue::PrivateLinkage );
+	U_TEST_ASSERT( !static_method->hasComdat() );
 }
 
 U_TEST( FunctionLinkage_Test1 )
@@ -79,26 +83,32 @@ U_TEST( FunctionLinkage_Test1 )
 	const llvm::Function* const foo= module->getFunction( "_Z3Foov" );
 	U_TEST_ASSERT( foo != nullptr );
 	U_TEST_ASSERT( foo->getLinkage() == llvm::GlobalValue::PrivateLinkage );
+	U_TEST_ASSERT( !foo->hasComdat() );
 
 	const llvm::Function* const baz= module->getFunction( "_ZN2NN3BazEv" );
 	U_TEST_ASSERT( baz != nullptr );
 	U_TEST_ASSERT( baz->getLinkage() == llvm::GlobalValue::PrivateLinkage );
+	U_TEST_ASSERT( !baz->hasComdat() );
 
 	const llvm::Function* const bar= module->getFunction( "_ZN1S3BarERKS_" );
 	U_TEST_ASSERT( bar != nullptr );
 	U_TEST_ASSERT( bar->getLinkage() == llvm::GlobalValue::PrivateLinkage );
+	U_TEST_ASSERT( !bar->hasComdat() );
 
 	const llvm::Function* const static_method= module->getFunction( "_ZN1S12StaticMethodEv" );
 	U_TEST_ASSERT( static_method != nullptr );
 	U_TEST_ASSERT( static_method->getLinkage() == llvm::GlobalValue::PrivateLinkage );
+	U_TEST_ASSERT( !static_method->hasComdat() );
 
 	const llvm::Function* const lol= module->getFunction( "_Z3Lolv" );
 	U_TEST_ASSERT( lol != nullptr );
 	U_TEST_ASSERT( lol->getLinkage() == llvm::GlobalValue::PrivateLinkage );
+	U_TEST_ASSERT( !lol->hasComdat() );
 
 	const llvm::Function* const kek= module->getFunction( "_ZN1S3KekERS_" );
 	U_TEST_ASSERT( kek != nullptr );
 	U_TEST_ASSERT( kek->getLinkage() == llvm::GlobalValue::PrivateLinkage );
+	U_TEST_ASSERT( !kek->hasComdat() );
 }
 
 U_TEST( FunctionLinkage_Test2 )
@@ -132,18 +142,22 @@ U_TEST( FunctionLinkage_Test2 )
 	const llvm::Function* const foo= module->getFunction( "_Z3Foov" );
 	U_TEST_ASSERT( foo != nullptr );
 	U_TEST_ASSERT( foo->getLinkage() == llvm::GlobalValue::ExternalLinkage );
+	U_TEST_ASSERT( !foo->hasComdat() );
 
 	const llvm::Function* const baz= module->getFunction( "_ZN2NN3BazEv" );
 	U_TEST_ASSERT( baz != nullptr );
 	U_TEST_ASSERT( baz->getLinkage() == llvm::GlobalValue::ExternalLinkage );
+	U_TEST_ASSERT( !baz->hasComdat() );
 
 	const llvm::Function* const bar= module->getFunction( "_ZN1S3BarERKS_" );
 	U_TEST_ASSERT( bar != nullptr );
 	U_TEST_ASSERT( bar->getLinkage() == llvm::GlobalValue::ExternalLinkage );
+	U_TEST_ASSERT( !bar->hasComdat() );
 
 	const llvm::Function* const static_method= module->getFunction( "_ZN1S12StaticMethodEv" );
 	U_TEST_ASSERT( static_method != nullptr );
 	U_TEST_ASSERT( static_method->getLinkage() == llvm::GlobalValue::ExternalLinkage );
+	U_TEST_ASSERT( !static_method->hasComdat() );
 }
 
 U_TEST( FunctionLinkage_Test3 )
@@ -170,10 +184,12 @@ U_TEST( FunctionLinkage_Test3 )
 	const llvm::Function* const foo= module->getFunction( "Foo" );
 	U_TEST_ASSERT( foo != nullptr );
 	U_TEST_ASSERT( foo->getLinkage() == llvm::GlobalValue::PrivateLinkage );
+	U_TEST_ASSERT( !foo->hasComdat() );
 
 	const llvm::Function* const bar= module->getFunction( "Bar" );
 	U_TEST_ASSERT( bar != nullptr );
 	U_TEST_ASSERT( bar->getLinkage() == llvm::GlobalValue::ExternalLinkage );
+	U_TEST_ASSERT( !bar->hasComdat() );
 }
 
 U_TEST( VariableLinkage_Test0 )
@@ -192,18 +208,22 @@ U_TEST( VariableLinkage_Test0 )
 	const llvm::GlobalVariable* const x= engine->FindGlobalVariableNamed( "x", true );
 	U_TEST_ASSERT( x != nullptr );
 	U_TEST_ASSERT( x->getLinkage() == llvm::GlobalValue::PrivateLinkage );
+	U_TEST_ASSERT( !x->hasComdat() );
 
 	const llvm::GlobalVariable* const y= engine->FindGlobalVariableNamed( "y", true );
 	U_TEST_ASSERT( y != nullptr );
 	U_TEST_ASSERT( y->getLinkage() == llvm::GlobalValue::PrivateLinkage );
+	U_TEST_ASSERT( !y->hasComdat() );
 
 	const llvm::GlobalVariable* const z= engine->FindGlobalVariableNamed( "z", true );
 	U_TEST_ASSERT( z != nullptr );
 	U_TEST_ASSERT( z->getLinkage() == llvm::GlobalValue::PrivateLinkage );
+	U_TEST_ASSERT( !z->hasComdat() );
 
 	const llvm::GlobalVariable* const w= engine->FindGlobalVariableNamed( "w", true );
 	U_TEST_ASSERT( w != nullptr );
 	U_TEST_ASSERT( w->getLinkage() == llvm::GlobalValue::PrivateLinkage );
+	U_TEST_ASSERT( !w->hasComdat() );
 }
 
 U_TEST( VariableLinkage_Test1 )
@@ -255,6 +275,7 @@ U_TEST( PolymorphClassesDataLinkage_Test0 )
 	const llvm::GlobalVariable* const vtable= engine->FindGlobalVariableNamed( "_ZTV1C", true );
 	U_TEST_ASSERT( vtable != nullptr );
 	U_TEST_ASSERT( vtable->getLinkage() == llvm::GlobalValue::PrivateLinkage );
+	U_TEST_ASSERT( !vtable->hasComdat() );
 }
 
 U_TEST( PolymorphClassesDataLinkage_Test1 )
@@ -273,6 +294,7 @@ U_TEST( PolymorphClassesDataLinkage_Test1 )
 	const llvm::GlobalVariable* const vtable= engine->FindGlobalVariableNamed( "_ZTV1C", true );
 	U_TEST_ASSERT( vtable != nullptr );
 	U_TEST_ASSERT( vtable->getLinkage() == llvm::GlobalValue::PrivateLinkage );
+	U_TEST_ASSERT( !vtable->hasComdat() );
 }
 
 U_TEST( PolymorphClassesDataLinkage_Test2 )
