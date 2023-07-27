7ffa06a0e0eefa644731b744ac5e6c696769d087
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-07-27 13:12:48 +0300

    Udaleny rudimenty "travis-ci".

--- ci/build_debug_llvm_src.sh
+++ ci/build_debug_llvm_src.sh
@@ -11,8 +11,8 @@ xz -d cmake-15.0.7.src.tar.xz &&\
 tar -xf cmake-15.0.7.src.tar &&\
 \
 # Configure build
-mkdir build-travis &&\
-cd build-travis &&\
+mkdir build_dir &&\
+cd build_dir &&\
 cmake ../source/ -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MODULE_PATH=$PWD/../cmake-15.0.7.src/Modules/ -DCMAKE_LINKER="ld.gold" -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=gold" -DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=gold" -DCMAKE_MODULE_LINKER_FLAGS="-fuse-ld=gold" -DLLVM_SRC_DIR=../llvm-15.0.7.src/ -DU_BUILD_COMPILER2=YES -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_BUILD_BENCHMARKS=OFF -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_BUILD_DOCS=OFF -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_BUILD_TESTS=OFF &&\
 \
 # Run build
@@ -20,6 +20,6 @@ cmake --build . -- -j 1 &&\
 \
 # Run ustlib tests
 cd .. &&\
-python3 source/annotated_tests_run.py --compiler-executable build-travis/compiler0/Compiler  --use-position-independent-code --input-dir source/ustlib_test &&\
-python3 source/annotated_tests_run.py --compiler-executable build-travis/compiler1/Compiler1 --use-position-independent-code --input-dir source/ustlib_test &&\
-python3 source/annotated_tests_run.py --compiler-executable build-travis/compiler2/Compiler2 --use-position-independent-code --input-dir source/ustlib_test
+python3 source/annotated_tests_run.py --compiler-executable build_dir/compiler0/Compiler  --use-position-independent-code --input-dir source/ustlib_test &&\
+python3 source/annotated_tests_run.py --compiler-executable build_dir/compiler1/Compiler1 --use-position-independent-code --input-dir source/ustlib_test &&\
+python3 source/annotated_tests_run.py --compiler-executable build_dir/compiler2/Compiler2 --use-position-independent-code --input-dir source/ustlib_test
--- ci/build_release_llvm_bin.sh
+++ ci/build_release_llvm_bin.sh
@@ -6,18 +6,18 @@ xz -d clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04.tar.xz &&\
 tar -xf clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04.tar &&\
 \
 # Configure build
-mkdir build-travis &&\
-cd build-travis &&\
+mkdir build_dir &&\
+cd build_dir &&\
 cmake ../source/ -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_LIB_DIR=../clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04/lib/ -DU_BUILD_COMPILER2=YES -DU_BUILD_COMPILER3=YES -DSPHINX_WARNINGS_AS_ERRORS=No -DU_BUILD_DOCS=No &&\
 \
 cmake --build . &&\
 \
 # Run ustlib tests
 cd .. &&\
-python3 source/annotated_tests_run.py --compiler-executable build-travis/compiler0/Compiler  --use-position-independent-code --input-dir source/ustlib_test &&\
-python3 source/annotated_tests_run.py --compiler-executable build-travis/compiler1/Compiler1 --use-position-independent-code --input-dir source/ustlib_test &&\
-python3 source/annotated_tests_run.py --compiler-executable build-travis/compiler2/Compiler2 --use-position-independent-code --input-dir source/ustlib_test &&\
-python3 source/annotated_tests_run.py --compiler-executable build-travis/compiler3/Compiler3 --use-position-independent-code --input-dir source/ustlib_test &&\
+python3 source/annotated_tests_run.py --compiler-executable build_dir/compiler0/Compiler  --use-position-independent-code --input-dir source/ustlib_test &&\
+python3 source/annotated_tests_run.py --compiler-executable build_dir/compiler1/Compiler1 --use-position-independent-code --input-dir source/ustlib_test &&\
+python3 source/annotated_tests_run.py --compiler-executable build_dir/compiler2/Compiler2 --use-position-independent-code --input-dir source/ustlib_test &&\
+python3 source/annotated_tests_run.py --compiler-executable build_dir/compiler3/Compiler3 --use-position-independent-code --input-dir source/ustlib_test &&\
 # install
 mkdir install &&\
-cmake --install build-travis --prefix install
+cmake --install build_dir --prefix install
