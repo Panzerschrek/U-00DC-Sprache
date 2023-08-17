fcc06da3b92b4458ab7b4192817a154b498a4743
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-08-17 10:20:47 +0300

    Sozdany fajly proekta "Language server".

---
+++ source/language_server/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+	std::ofstream log_file( "C:/Users/user/Documents/Projects/other/U-00DC-Sprache/other/sprache_lang_server.txt", std::ios::app );
+
+	while(true)
+	{
+	}
+}
