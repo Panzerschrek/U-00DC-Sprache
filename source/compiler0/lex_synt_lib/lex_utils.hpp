91b79aa1483293ee65e25ada1fc38c69ecabf37c
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-08-21 09:01:27 +0300

    Vynos nekotorogo vspomogateljnogo koda v otdeljnyj fajl.

---
+++ source/compiler0/lex_synt_lib/lex_utils.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <optional>
+#include "lexical_analyzer.hpp"
+
+namespace U
+{
+
+// Complexity is linear.
+std::optional<SrcLoc> GetLexemSrcLocForPosition( const uint32_t line, const uint32_t column, const Lexems& lexems );
+
+} // namespace U
--- source/language_server/document.cpp
+++ source/language_server/document.cpp
@@ -1,4 +1,5 @@
 #include "../code_builder_lib_common/source_file_contents_hash.hpp"
+#include "../compiler0/lex_synt_lib/lex_utils.hpp"
 #include "../compiler0/lex_synt_lib/syntax_analyzer.hpp"
 #include "../tests/tests_common.hpp"
 #include "document.hpp"
@@ -8,34 +9,6 @@ namespace U
 
 namespace LangServer
 {
-
-namespace
-{
-
-// Complexity is linear.
-std::optional<SrcLoc> GetLexemSrcLocForPosition( const uint32_t line, const uint32_t column, const Lexems& lexems )
-{
-	// TODO - return none, if position is between lexems.
-
-	const SrcLoc pos_loc( 0, line, column );
-	auto it= lexems.begin();
-	for( ; it < lexems.end(); ++it )
-	{
-		// Compare without file index and macro expansion context.
-		const SrcLoc lexem_loc( 0, it->src_loc.GetLine(), it->src_loc.GetColumn() );
-		if( pos_loc == lexem_loc )
-			return lexem_loc;
-		if( pos_loc < lexem_loc )
-		{
-			if( it != lexems.begin() )
-				return std::prev(it)->src_loc;
-		}
-	}
-
-	return std::nullopt;
-}
-
-} // namespace
 
 Document::Document( std::ostream& log, std::string text )
 	: log_(log)
