f0b3f2f16d3d96689e8e8046616606d3a2581752
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-08-23 10:21:07 +0300

    Iz Plagina dlä "QtCreator" udalön funkçional postrojenija i pokaza dereva simvolov dokumenta. On teperj ne nužen, t. k. teperj jestj jazykovoj server dlä Ü, kotoryj umejet stroitj simvoly, a v "QtCreator" jestj klijent etogo servera, kotoryj eti simvoly umejet otobražatj.

--- source/qt_creator_plugin/editor_widget.hpp
+++ source/qt_creator_plugin/editor_widget.hpp
@@ -1,33 +1,11 @@
 #pragma once
-#include <QComboBox>
-#include <QTimer>
-#include <QTreeView>
-
 #include <plugins/texteditor/texteditor.h>
-
-#include "program_model.hpp"
-#include "outline_widget_model.hpp"
 
 namespace U
 {
 
 namespace QtCreatorPlugin
 {
-
-class OutlineTreeViewComboBox : public QComboBox
-{
-	Q_OBJECT
-
-public:
-	explicit OutlineTreeViewComboBox( QWidget* parent = nullptr );
-
-	virtual bool eventFilter( QObject* object, QEvent* event ) override;
-	virtual void hidePopup() override;
-
-private:
-	QTreeView view_;
-	bool skip_next_hide_= false;
-};
 
 class EditorWidget final : public TextEditor::TextEditorWidget
 {
@@ -41,19 +19,6 @@ private:
 
 protected:
 	virtual void contextMenuEvent( QContextMenuEvent* event ) override;
-
-private:
-	void OnTextChanged();
-	void OnTimerExpired();
-	void OnItemActivated();
-	void OnCursorPositionChanged();
-
-private:
-	QTimer timer_;
-	OutlineTreeViewComboBox combo_box_;
-	OutlineWidgetModel combo_box_model_;
-	ProgramModelPtr program_model_;
-	bool block_cursor_sync_= false;
 };
 
 } // namespace QtCreatorPlugin
