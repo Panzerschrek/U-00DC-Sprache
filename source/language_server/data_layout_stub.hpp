63506696c0eab4f4737a311772dc6f0d698fc53d
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-09-15 16:18:33 +0300

    Dobavleny zagluški dlä "DataLayout".

--- source/language_server/data_layout_stub.hpp
+++ source/language_server/data_layout_stub.hpp
@@ -10,10 +10,10 @@ namespace LangServer
 {
 
 // Normally DataLaout is obtained via llvm::TargetMachine.
-// But doing so requires linking against at least one target linrary, that drasticly increases result language server executable size.
+// But doing so requires linking against at least one target linrary, that drastically increases result language server executable size.
 // So, use our own target data layout construction stub.
 // It is not so bad to construct somehow incorrect data layout, since this doesn't affect any real compilation.
-// Compiler frontent uses only few data layout features, like pointer size, types alignment and endianness.
+// Compiler frontend uses only few data layout features, like pointer size, types alignment and endianness.
 llvm::DataLayout CreateStubDataLayout( const std::string& target_triple_str );
 
 } // namespace LangServer
