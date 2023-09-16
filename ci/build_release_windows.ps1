173e7cba7999e3b57a4332ba964c7cc128171803
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-09-16 09:57:20 +0300

    Popytka taki zastavitj "ninja" rabotatj.

--- ci/build_release_windows.ps1
+++ ci/build_release_windows.ps1
@@ -9,7 +9,14 @@ Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/l
 7z x cmake-15.0.7.src.tar.xz
 7z x cmake-15.0.7.src.tar
 
-cmd 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat' amd64
+# Messy stuff. Call vcvarsall.bat, extract all environment variable prepared in this call and redefine them in context of this powershell script.
+cmd /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat`" && set > %temp%\vcvars.txt"
+Get-Content "$env:temp\vcvars.txt" | Foreach-Object {
+	if ($_ -match "^(.*?)=(.*)$") {
+		Set-Content "env:\$($matches[1])" $matches[2]
+	}
+}
+
 mkdir build_dir
-cmake -S source -B build_dir -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH="$curDir/cmake-15.0.7.src/Modules/" -DLLVM_SRC_DIR="$curDir/llvm-15.0.7.src/" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_BUILD_BENCHMARKS=OFF -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_BUILD_DOCS=OFF -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_BUILD_TESTS=OFF -DU_BUILD_UNICODE_FILE_NAMES_TEST=OFF
+cmake -S source -B build_dir  -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH="$curDir/cmake-15.0.7.src/Modules/" -DLLVM_SRC_DIR="$curDir/llvm-15.0.7.src/" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_BUILD_BENCHMARKS=OFF -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_BUILD_DOCS=OFF -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_BUILD_TESTS=OFF -DU_BUILD_UNICODE_FILE_NAMES_TEST=OFF
 cmake --build build_dir
