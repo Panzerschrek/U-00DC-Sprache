50ebaac28a0cd89ff00fc703e27e1aa1faa422fc
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-09-16 11:35:54 +0300

    Zapuskajem "cmake install".

--- ci/build_release_windows.ps1
+++ ci/build_release_windows.ps1
@@ -2,6 +2,7 @@
 $curDir = Get-Location
 $curDir = $curDir -replace '\\', '/'
 
+# Obtain LLVM
 Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.7/llvm-15.0.7.src.tar.xz" -OutFile "llvm-15.0.7.src.tar.xz"
 Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.7/cmake-15.0.7.src.tar.xz" -OutFile "cmake-15.0.7.src.tar.xz"
 7z x llvm-15.0.7.src.tar.xz
@@ -17,6 +18,11 @@ Get-Content "$env:temp\vcvars.txt" | Foreach-Object {
 	}
 }
 
+# Perform cmake preparation and build
 mkdir build_dir
 cmake -S source -B build_dir  -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH="$curDir/cmake-15.0.7.src/Modules/" -DLLVM_SRC_DIR="$curDir/llvm-15.0.7.src/" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_BUILD_BENCHMARKS=OFF -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_BUILD_DOCS=OFF -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_BUILD_TESTS=OFF -DU_BUILD_COMPILER2=ON -DU_BUILD_COMPILER3=ON -DU_BUILD_PY_TESTS=OFF -DU_BUILD_UNICODE_FILE_NAMES_TEST=OFF
 cmake --build build_dir
+
+# install
+mkdir install
+cmake --install build_dir --prefix install
