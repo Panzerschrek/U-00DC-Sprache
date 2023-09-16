8dc85f62846c492ad1e5dcfe4c9c5760b493d82e
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-09-16 09:10:27 +0300

    Pytajemsä ukazyvatj polnyje puti k zavisimostäm.

--- ci/build_release_windows.ps1
+++ ci/build_release_windows.ps1
@@ -1,3 +1,5 @@
+$curDir = Get-Location
+
 Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.7/llvm-15.0.7.src.tar.xz" -OutFile "llvm-15.0.7.src.tar.xz"
 Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.7/cmake-15.0.7.src.tar.xz" -OutFile "cmake-15.0.7.src.tar.xz"
 7z x llvm-15.0.7.src.tar.xz
@@ -7,5 +9,5 @@ Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/l
 
 cmd 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat' amd64
 mkdir build_dir
-cmake -S source -B build_dir -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH="cmake-15.0.7.src/Modules/" -DLLVM_SRC_DIR="llvm-15.0.7.src/" -DLLVM_TARGETS_TO_BUILD="x86" -DLLVM_BUILD_BENCHMARKS=OFF -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_BUILD_DOCS=OFF -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_BUILD_TESTS=OFF -DU_BUILD_UNICODE_FILE_NAMES_TEST=OFF
+cmake -S source -B build_dir -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH="$curDir/cmake-15.0.7.src/Modules/" -DLLVM_SRC_DIR="$curDir/llvm-15.0.7.src/" -DLLVM_TARGETS_TO_BUILD="x86" -DLLVM_BUILD_BENCHMARKS=OFF -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_BUILD_DOCS=OFF -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_BUILD_TESTS=OFF -DU_BUILD_UNICODE_FILE_NAMES_TEST=OFF
 cmake --build build_dir
