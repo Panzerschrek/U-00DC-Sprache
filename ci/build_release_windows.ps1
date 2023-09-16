2d19d40667edfa58c07785181ebfe69a1367a43b
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-09-16 09:20:10 +0300

    Popravleno ukazanije çeli "X86".

--- ci/build_release_windows.ps1
+++ ci/build_release_windows.ps1
@@ -11,5 +11,5 @@ Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/l
 
 cmd 'C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat' amd64
 mkdir build_dir
-cmake -S source -B build_dir -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH="$curDir/cmake-15.0.7.src/Modules/" -DLLVM_SRC_DIR="$curDir/llvm-15.0.7.src/" -DLLVM_TARGETS_TO_BUILD="x86" -DLLVM_BUILD_BENCHMARKS=OFF -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_BUILD_DOCS=OFF -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_BUILD_TESTS=OFF -DU_BUILD_UNICODE_FILE_NAMES_TEST=OFF
+cmake -S source -B build_dir -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH="$curDir/cmake-15.0.7.src/Modules/" -DLLVM_SRC_DIR="$curDir/llvm-15.0.7.src/" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_BUILD_BENCHMARKS=OFF -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_BUILD_DOCS=OFF -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_BUILD_TESTS=OFF -DU_BUILD_UNICODE_FILE_NAMES_TEST=OFF
 cmake --build build_dir
