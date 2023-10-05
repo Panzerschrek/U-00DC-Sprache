58a4e33a28da20c3ec5765ab0a28a95359a733e5
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-10-05 11:26:37 +0300

    Dobavleny koje-kakije testy. Vyjavlen defekt kompilätora pri vozvrate znacenija.

--- source/tests/cpp_tests/destructors_test.cpp
+++ source/tests/cpp_tests/destructors_test.cpp
@@ -3209,6 +3209,130 @@ U_TEST(SwitchOperatorDestructors_Test0)
 	g_destructors_call_sequence.clear();
 }
 
+U_TEST( DerivedToBaseConversion_Destructors_Test0 )
+{
+	static const char c_program_text[]=
+	R"(
+		fn DestructorCalled(i32 x);
+		class Base polymorph
+		{
+			i32 x;
+			fn constructor( i32 in_x ) ( x= in_x ) {  DestructorCalled( -x ); }
+			fn constructor( mut this, Base& other ) ( x= other.x ) { DestructorCalled( -x * 3 ); }
+			fn destructor() { DestructorCalled(x); }
+		}
+		class Derived : Base
+		{
+			fn constructor( i32 in_x ) ( base( in_x ) ) { DestructorCalled( -x * 2 ); }
+			fn constructor( mut this, Derived& other ) ( base( cast_ref</Base/>(other) ) ) { DestructorCalled( -x * 6 ); }
+			fn destructor()
+			{
+				DestructorCalled( x * 2 );
+			}
+		}
+		fn Bar( Base b ){ } // Destroy here base.
+		fn Foo()
+		{
+			// Construct derived (and base inside), copy-construct base, destroy derived, destroy base function argument.
+			Bar( Derived( 33 ) );
+		}
+	)";
+
+	const EnginePtr engine= CreateEngine( BuildProgram( c_program_text ) );
+	DestructorTestPrepare(engine);
+
+	llvm::Function* const function= engine->FindFunctionNamed( "_Z3Foov" );
+	U_TEST_ASSERT( function != nullptr );
+
+	engine->runFunction( function, {} );
+
+	U_TEST_ASSERT( g_destructors_call_sequence == std::vector<int>( { -33, -66,  -99,  66, 33,  33 } ) );
+}
+
+U_TEST( DerivedToBaseConversion_Destructors_Test1 )
+{
+	static const char c_program_text[]=
+	R"(
+		fn DestructorCalled(i32 x);
+		class Base polymorph
+		{
+			i32 x;
+			fn constructor( i32 in_x ) ( x= in_x ) {  DestructorCalled( -x ); }
+			fn constructor( mut this, Base& other ) ( x= other.x ) { DestructorCalled( -x * 3 ); }
+			fn destructor() { DestructorCalled(x); }
+		}
+		class Derived : Base
+		{
+			fn constructor( i32 in_x ) ( base( in_x ) ) { DestructorCalled( -x * 2 ); }
+			fn constructor( mut this, Derived& other ) ( base( cast_ref</Base/>(other) ) ) { DestructorCalled( -x * 6 ); }
+			fn destructor()
+			{
+				DestructorCalled( x * 2 );
+			}
+		}
+		fn Bar( Base b ){ } // Destroy here base.
+		fn Foo()
+		{
+			var Derived derived( 33 ); // Construct derived (and base inside).
+			Bar( derived ); // Copy-construct base, destroy base function argument.
+			// Destroy derived.
+		}
+	)";
+
+	const EnginePtr engine= CreateEngine( BuildProgram( c_program_text ) );
+	DestructorTestPrepare(engine);
+
+	llvm::Function* const function= engine->FindFunctionNamed( "_Z3Foov" );
+	U_TEST_ASSERT( function != nullptr );
+
+	engine->runFunction( function, {} );
+
+	U_TEST_ASSERT( g_destructors_call_sequence == std::vector<int>( { -33, -66,  -99,  33, 66,  33 } ) );
+}
+
+U_TEST( DerivedToBaseConversion_Destructors_Test2 )
+{
+	static const char c_program_text[]=
+	R"(
+		fn DestructorCalled(i32 x);
+		class Base polymorph
+		{
+			i32 x;
+			fn constructor( i32 in_x ) ( x= in_x ) {  DestructorCalled( -x ); }
+			fn constructor( mut this, Base& other ) ( x= other.x ) { DestructorCalled( -x * 3 ); }
+			fn destructor() { DestructorCalled(x); }
+		}
+		class Derived : Base
+		{
+			fn constructor( i32 in_x ) ( base( in_x ) ) { DestructorCalled( -x * 2 ); }
+			fn constructor( mut this, Derived& other ) ( base( cast_ref</Base/>(other) ) ) { DestructorCalled( -x * 6 ); }
+			fn destructor()
+			{
+				DestructorCalled( x * 2 );
+			}
+		}
+		fn Bar() : Base
+		{
+			// Construct derived (and base inside), copy-construct base, destroy derived.
+			return Derived( 33 );
+		}
+		fn Foo()
+		{
+			Bar();
+		}
+	)";
+
+	const EnginePtr engine= CreateEngine( BuildProgram( c_program_text ) );
+	DestructorTestPrepare(engine);
+
+	llvm::Function* const function= engine->FindFunctionNamed( "_Z3Foov" );
+	U_TEST_ASSERT( function != nullptr );
+
+	engine->runFunction( function, {} );
+
+	U_TEST_ASSERT( g_destructors_call_sequence == std::vector<int>( { -33, -66,  -99,  66, 33,  33 } ) );
+}
+
 } // namespace
 
 } // namespace U
