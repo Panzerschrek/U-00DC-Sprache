dc19e5f7d40c681d60d62e9535605ee1167b0525
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-10-05 16:19:14 +0300

    Dobavlen räd testov.

--- source/tests/py_tests/byval_this.py
+++ source/tests/py_tests/byval_this.py
@@ -115,16 +115,192 @@ def ByValThis_Test4():
 			fn constructor( f32 in_x, f32 in_y ) ( x(in_x), y(in_y) ) {}
 			fn constructor( mut this, Vec& other )= delete;
 			fn scale( byval mut this, f32 s ) : Vec { x *= s; y *= s; return move(this); }
-			fn reverse( byval mut this ) : Vec{ x= -x; y= -y; return move(this); }
+			fn reverse( byval mut this ) : Vec { x= -x; y= -y; return move(this); }
+			fn swap_components( byval mut this ) : Vec { auto tmp= x; x= y; y=tmp; return move(this); }
 		}
 		static_assert( !typeinfo</Vec/>.is_copy_constructible );
 		fn Foo()
 		{
 			// Use chain of byval thiscall methods, starting with temp variable construction.
-			auto v= Vec( 3.0f, 5.0f ).scale( 2.0f ).reverse();
-			halt if( v.x !=  -6.0f );
-			halt if( v.y != -10.0f );
+			auto v= Vec( 3.0f, 5.0f ).scale( 2.0f ).reverse().swap_components();
+			halt if( v.x != -10.0f );
+			halt if( v.y !=  -6.0f );
 		}
 	"""
 	tests_lib.build_program( c_program_text )
 	tests_lib.run_function( "_Z3Foov" )
+
+
+def ByValThis_Test5():
+	c_program_text= """
+		struct S
+		{
+			i32 x;
+			fn Foo( byval this ) : i32
+			{
+				return Bar(); // Call reference-thiscall method using value this.
+			}
+			fn Bar( this ) : i32 { return x * 11; }
+		}
+		fn Foo() : i32
+		{
+			var S s{ .x= 7 };
+			return s.Foo();
+		}
+	"""
+	tests_lib.build_program( c_program_text )
+	tests_lib.run_function( "_Z3Foov" )
+	call_result= tests_lib.run_function( "_Z3Foov" )
+	assert( call_result == 77 )
+
+
+def ByValThis_Test6():
+	c_program_text= """
+		struct S
+		{
+			i32 x;
+			fn Foo( byval mut this ) : i32
+			{
+				Reserve(); // Call mutable reference-thiscall method using value this.
+				return x;
+			}
+			fn Reserve( mut this ) { x= -x; }
+		}
+		fn Foo() : i32
+		{
+			var S s{ .x= -765 };
+			return s.Foo();
+		}
+	"""
+	tests_lib.build_program( c_program_text )
+	tests_lib.run_function( "_Z3Foov" )
+	call_result= tests_lib.run_function( "_Z3Foov" )
+	assert( call_result == 765 )
+
+
+def ByValThis_Test7():
+	c_program_text= """
+		struct S
+		{
+			i32 x;
+			fn Foo( byval mut this ) : i32
+			{
+				Reserve( this ); // Call mutable reference function using value this.
+				return x;
+			}
+		}
+		fn Reserve( S &mut s ) { s.x= -s.x; }
+		fn Foo() : i32
+		{
+			var S s{ .x= -88877 };
+			return s.Foo();
+		}
+	"""
+	tests_lib.build_program( c_program_text )
+	tests_lib.run_function( "_Z3Foov" )
+	call_result= tests_lib.run_function( "_Z3Foov" )
+	assert( call_result == 88877 )
+
+
+def ByValThis_Test8():
+	c_program_text= """
+		class Base polymorph
+		{
+			fn Foo( byval this );
+			fn constructor( mut this, Base& other )= default;
+		}
+		class Derived : Base {}
+		fn Foo()
+		{
+			// This is equivalent to someting like Base::Foo( Derived() );
+			// Move here inpossible since types are different, so, copy constructor is called instead,
+			Derived().Foo();
+		}
+	"""
+	tests_lib.build_program( c_program_text )
+
+
+def ByValThisErrors_Test0():
+	c_program_text= """
+		struct S
+		{
+			i32 x;
+			fn Foo( byval this )
+			{
+				x= 42; // error - 'this' is not mutable.
+			}
+		}
+
+	"""
+	errors_list= ConvertErrors( tests_lib.build_program_with_errors( c_program_text ) )
+	assert( len(errors_list) > 0 )
+	assert( HaveError( errors_list, "ExpectedReferenceValue", 7 ) )
+
+
+def ByValThisErrors_Test1():
+	c_program_text= """
+		struct S
+		{
+			i32 x;
+			fn Foo( byval this )
+			{
+				move( this ); // error - 'this' is not mutable.
+			}
+		}
+	"""
+	errors_list= ConvertErrors( tests_lib.build_program_with_errors( c_program_text ) )
+	assert( len(errors_list) > 0 )
+	assert( HaveError( errors_list, "ExpectedReferenceValue", 7 ) )
+
+
+def ByValThisErrors_Test2():
+	c_program_text= """
+		struct S
+		{
+			i32 x;
+			fn Foo( byval this )
+			{
+				Bar(); // Error - 'this' is not mutable, but mutable method is called for it.
+			}
+			fn Bar( mut this );
+		}
+	"""
+	errors_list= ConvertErrors( tests_lib.build_program_with_errors( c_program_text ) )
+	assert( len(errors_list) > 0 )
+	assert( HaveError( errors_list, "CouldNotSelectOverloadedFunction", 7 ) )
+
+
+def ByValThisErrors_Test3():
+	c_program_text= """
+		struct S
+		{
+			i32 x;
+			fn Foo( byval this )
+			{
+				Bar(this); // Error - 'this' is not mutable, but mutable function is called for it.
+			}
+		}
+		fn Bar( S &mut s );
+	"""
+	errors_list= ConvertErrors( tests_lib.build_program_with_errors( c_program_text ) )
+	assert( len(errors_list) > 0 )
+	assert( HaveError( errors_list, "CouldNotSelectOverloadedFunction", 7 ) )
+
+
+def ByValThisErrors_Test4():
+	c_program_text= """
+		class Base polymorph
+		{
+			fn Foo( byval this );
+		}
+		class Derived : Base {}
+		fn Foo()
+		{
+			// This is equivalent to someting like Base::Foo( Derived() );
+			// But such call is impossible - move is disabled, since types are different and copying is impossible since 'Base' is non-copyable.
+			Derived().Foo();
+		}
+	"""
+	errors_list= ConvertErrors( tests_lib.build_program_with_errors( c_program_text ) )
+	assert( len(errors_list) > 0 )
+	assert( HaveError( errors_list, "CopyConstructValueOfNoncopyableType", 11 ) )
