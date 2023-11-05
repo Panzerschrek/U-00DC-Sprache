fbc0a4bbd2600a16e7b68095a7592e4eafba8cfd
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-11-05 15:09:47 +0300

    Docinka sborki.

--- ci/build_release_llvm_bin.sh
+++ ci/build_release_llvm_bin.sh
@@ -8,10 +8,16 @@ tar -xf clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04.tar &&\
 # Configure build
 mkdir build_dir &&\
 cd build_dir &&\
-cmake ../source/ -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_LIB_DIR=../clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04/lib/ -DU_BUILD_COMPILER2=NO -DU_BUILD_COMPILER3=NO -DSPHINX_WARNINGS_AS_ERRORS=NO -DU_BUILD_DOCS=YES &&\
+cmake ../source/ -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_LIB_DIR=../clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04/lib/ -DU_BUILD_COMPILER2=YES -DU_BUILD_COMPILER3=YES -DSPHINX_WARNINGS_AS_ERRORS=NO -DU_BUILD_DOCS=YES &&\
 \
 cmake --build . &&\
 \
+# Run ustlib tests
+cd .. &&\
+python3 source/annotated_tests_run.py --compiler-executable build_dir/compiler0/Compiler  --use-position-independent-code --input-dir source/ustlib_test &&\
+python3 source/annotated_tests_run.py --compiler-executable build_dir/compiler1/Compiler1 --use-position-independent-code --input-dir source/ustlib_test &&\
+python3 source/annotated_tests_run.py --compiler-executable build_dir/compiler2/Compiler2 --use-position-independent-code --input-dir source/ustlib_test &&\
+python3 source/annotated_tests_run.py --compiler-executable build_dir/compiler3/Compiler3 --use-position-independent-code --input-dir source/ustlib_test &&\
 # install
 mkdir install &&\
 cmake --install build_dir --prefix install
