a32cdb5d7c0dd6a2dcac11dd10b47d9e94e5bc6c
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-11-25 16:26:28 +0300

    Sozdana zadaca dlä "Github Actions" dlä sborki so vklücönnym ucötom pokrytija koda i generaçii itogovogo otcöta.

---
+++ ci/calculate_code_coverage.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Get llvm
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.6/clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04.tar.xz &&\
+xz -d clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04.tar.xz &&\
+tar -xf clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04.tar &&\
+\
+# Configure build
+mkdir build_dir &&\
+cd build_dir &&\
+cmake ../source/ -G Ninja -DCMAKE_BUILD_TYPE=Debug -DLLVM_LIB_DIR=../clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04/lib/ -DU_BUILD_COMPILER2=YES -DU_BUILD_COMPILER3=NO -DU_BUILD_DOCS=NO -DU_COVERAGE_ENABLED=YES &&\
+\
+cmake --build . &&\
+\
+# Run ustlib tests
+cd .. &&\
+python3 source/annotated_tests_run.py --compiler-executable build_dir/compiler0/Compiler  --use-position-independent-code --input-dir source/ustlib_test &&\
+python3 source/annotated_tests_run.py --compiler-executable build_dir/compiler1/Compiler1 --use-position-independent-code --input-dir source/ustlib_test &&\
+python3 source/annotated_tests_run.py --compiler-executable build_dir/compiler2/Compiler2 --use-position-independent-code --input-dir source/ustlib_test &&\
+# Run the coverage preparation code
+lcov -o code_coverage.info -c -d build_dir &&\
+genhtml -o code_coverage_report code_coverage.info
+
+
