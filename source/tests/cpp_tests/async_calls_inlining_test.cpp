13ac73ddda10e5f89cd0bdd733dc269275992185
Panzerschrek <AntiPanzerschrek@yandex.ru>
2023-11-27 15:30:34 +0300

    Dobavlen test na nalicije/otsutstvije "yield" vo vstraivajemoj funkçii.

--- source/tests/cpp_tests/async_calls_inlining_test.cpp
+++ source/tests/cpp_tests/async_calls_inlining_test.cpp
@@ -11,9 +11,9 @@ U_TEST(AsyncCallInlining_Test0)
 {
 	static const char c_program_text[]=
 	R"(
+		// Simplest async function.
 		fn async GetX() : i32
 		{
-			yield;
 			return 76767;
 		}
 		fn async GetXWrapper() : i32
@@ -40,6 +40,42 @@ U_TEST(AsyncCallInlining_Test0)
 
 	engine->runFunction( function, llvm::ArrayRef<llvm::GenericValue>() );
 }
+
+U_TEST(AsyncCallInlining_Test1)
+{
+	static const char c_program_text[]=
+	R"(
+		// Async function with "yield" inside.
+		fn async GetX() : u32
+		{
+			yield;
+			return 3433u;
+		}
+		fn async GetXWrapper() : u32
+		{
+			return GetX().await;
+		}
+		fn Foo()
+		{
+			auto mut f= GetXWrapper();
+			loop
+			{
+				if_coro_advance( x : f )
+				{
+					halt if( x != 3433u );
+					break;
+				}
+			}
+		}
+	)";
+
+	const EnginePtr engine= CreateEngine( BuildProgramForAsyncFunctionsInliningTest( c_program_text ) );
+	llvm::Function* function= engine->FindFunctionNamed( "_Z3Foov" );
+	U_TEST_ASSERT( function != nullptr );
+
+	engine->runFunction( function, llvm::ArrayRef<llvm::GenericValue>() );
+}
+
 
 } // namespace
 
