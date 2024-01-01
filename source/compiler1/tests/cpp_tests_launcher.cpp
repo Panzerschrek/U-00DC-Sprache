4ef19fb0fa2b6c27de5f5a1502eef491c34140a9
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-01-01 12:23:41 +0300

    Kodirujem v lämbdah indeksy "tuple for", v kotoryh eti lämbdy nahodätsä.

--- source/compiler1/tests/cpp_tests_launcher.cpp
+++ source/compiler1/tests/cpp_tests_launcher.cpp
@@ -67,8 +67,6 @@ bool FilterTest( const std::string& test_name )
 
 	static const std::unordered_set<std::string> c_test_to_disable
 	{
-		"LambdasMangling_Test9",
-		"LambdasMangling_Test10",
 	};
 
 	return c_test_to_disable.count( test_name_without_file_name ) == 0;
