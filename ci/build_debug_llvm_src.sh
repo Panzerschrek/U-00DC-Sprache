d4bda448d52275b3f1e98b884c980a5bfd2c0a4b
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-01-20 15:53:31 +0300

    V otladocnoj "CI" sborke sobirajem takže "LLD".

--- ci/build_debug_llvm_src.sh
+++ ci/build_debug_llvm_src.sh
@@ -9,11 +9,21 @@ tar -xf llvm-15.0.7.src.tar &&\
 wget https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.7/cmake-15.0.7.src.tar.xz &&\
 xz -d cmake-15.0.7.src.tar.xz &&\
 tar -xf cmake-15.0.7.src.tar &&\
+# Get lld
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.7/lld-15.0.7.src.tar.xz &&\
+xz -d lld-15.0.7.src.tar.xz &&\
+tar -xf lld-15.0.7.src.tar &&\
+# Get libunwind
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.7/libunwind-15.0.7.src.tar.xz &&\
+xz -d libunwind-15.0.7.src.tar.xz &&\
+tar -xf libunwind-15.0.7.src.tar &&\
 \
+# lld Mach-O library includes libunwind using path relative to llvm src directory and without a version. So, rename it.
+mv libunwind-15.0.7.src libunwind &&\
 # Configure build
 mkdir build_dir &&\
 cd build_dir &&\
-cmake ../source/ -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MODULE_PATH=$PWD/../cmake-15.0.7.src/Modules/ -DCMAKE_LINKER="ld.gold" -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=gold" -DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=gold" -DCMAKE_MODULE_LINKER_FLAGS="-fuse-ld=gold" -DLLVM_SRC_DIR=../llvm-15.0.7.src/ -DU_BUILD_COMPILER2=YES -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_NATIVE_ARCH="X86" -DLLVM_BUILD_BENCHMARKS=OFF -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_BUILD_DOCS=OFF -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_BUILD_TESTS=OFF &&\
+cmake ../source/ -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MODULE_PATH=$PWD/../cmake-15.0.7.src/Modules/ -DCMAKE_LINKER="ld.gold" -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=gold" -DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=gold" -DCMAKE_MODULE_LINKER_FLAGS="-fuse-ld=gold" -DLLVM_SRC_DIR=../llvm-15.0.7.src/ -DLLVM_EXTERNAL_LLD_SOURCE_DIR=../lld-15.0.7.src/ -DLLVM_TOOL_LLD_BUILD=ON -DU_BUILD_COMPILER2=YES -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_NATIVE_ARCH="X86" -DLLVM_BUILD_BENCHMARKS=OFF -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_BUILD_DOCS=OFF -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_BUILD_TESTS=OFF &&\
 \
 # Run build
 cmake --build . -- -j 1 &&\
