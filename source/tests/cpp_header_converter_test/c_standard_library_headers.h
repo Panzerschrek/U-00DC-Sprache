0ac1fcf7416c0196539159a74d86fa2e10ad4796
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-07-30 13:30:22 +0300

    Pytajemsä preobrazovyvatj (pocti) vsü standartnuju biblioteku "C". Poka cto eto ne ocenj vyhodit - nužny nekotoryje vstrojennyje zagolovocnyje fajly ot "clang" i ot "gcc". K tomu že slucajutsä konflikty imön.

---
+++ source/tests/cpp_header_converter_test/c_standard_library_headers.h
@@ -0,0 +1,31 @@
+#include <assert.h>
+#include <complex.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fenv.h>
+#include <float.h>
+#include <inttypes.h>
+#include <iso646.h>
+#include <limits.h>
+#include <locale.h>
+#include <math.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdalign.h>
+#include <stdarg.h>
+#include <stdatomic.h>
+// #include <stdbit.h> // C23
+#include <stdbool.h>
+// #include <stdckdint.h> // C23
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdnoreturn.h>
+#include <string.h>
+#include <tgmath.h>
+// #include <threads.h> // C11, but unsupported by GCC.
+#include <time.h>
+#include <uchar.h>
+#include <wchar.h>
+#include <wctype.h>
