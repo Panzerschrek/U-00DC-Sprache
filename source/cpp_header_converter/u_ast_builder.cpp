c0087331b2d0a6da59fec9168f8bbf5834367aeb
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-07-31 19:28:01 +0300

    Docinka sborki pod "Windows".

--- source/cpp_header_converter/u_ast_builder.cpp
+++ source/cpp_header_converter/u_ast_builder.cpp
@@ -529,7 +529,7 @@ std::string CppAstConsumer::TranslateIdentifier( const llvm::StringRef identifie
 		return ( "ü" + identifier ).str();
 
 	// Avoid using keywords as names.
-	if( IsKeyword( StringViewToStringRef( identifier ) ) )
+	if( IsKeyword( StringRefToStringView( identifier ) ) )
 		return (identifier + "_").str();
 
 	return identifier.str();
