113904db7be553510af7773e79235be69a16f0da
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-10-22 10:21:39 +0300

    Dobavleny jescö testy.

--- source/tests/py_tests/second_order_references_inside_structs_test.py
+++ source/tests/py_tests/second_order_references_inside_structs_test.py
@@ -122,3 +122,69 @@ def ReferenceProtectionError_ForSecondOrderInnerReference_Test5():
 	assert( len(errors_list) > 0 )
 	assert( not HasError( errors_list, "ReferenceProtectionError", 11 ) )
 	assert( HasError( errors_list, "ReferenceProtectionError", 12 ) )
+
+
+def ReferenceProtectionError_ForSecondOrderInnerReference_Test6():
+	c_program_text= """
+		struct A{ i32 &mut x; }
+		struct B{ A   &imut a; }
+		struct BWrapper{ B b; }
+		fn Foo()
+		{
+			var i32 mut x= 0;
+			var A a{ .x= x };
+			var BWrapper b_wrapper{ .b{ .a= a } };
+
+			var A& a0= b_wrapper.b.a; // "a0.x" points to "a.x".
+			var A& a1= b_wrapper.b.a; // Error - creating second node pointing to "a.x" inside "a1".
+		}
+	"""
+	errors_list= ConvertErrors( tests_lib.build_program_with_errors( c_program_text ) )
+	assert( len(errors_list) > 0 )
+	assert( not HasError( errors_list, "ReferenceProtectionError", 11 ) )
+	assert( HasError( errors_list, "ReferenceProtectionError", 12 ) )
+
+
+def ReferenceProtectionError_ForSecondOrderInnerReference_Test7():
+	c_program_text= """
+		struct A{ i32 &mut x; }
+		struct B{ A   &imut a; }
+		struct BWrapper{ B b; }
+		fn Foo()
+		{
+			var i32 mut x= 0;
+			var A a{ .x= x };
+			var BWrapper b_wrapper{ .b{ .a= a } };
+
+			auto &x_ref= a.x; // "x_ref" points to "a.x" inner reference node.
+			var A &a_ref= b_wrapper.b.a; // Error - creating second node pointing to "a.x" inside "a_ref".
+		}
+	"""
+	errors_list= ConvertErrors( tests_lib.build_program_with_errors( c_program_text ) )
+	assert( len(errors_list) > 0 )
+	assert( not HasError( errors_list, "ReferenceProtectionError", 11 ) )
+	assert( HasError( errors_list, "ReferenceProtectionError", 12 ) )
+
+
+def ReferenceProtectionError_ForSecondOrderInnerReference_Test8():
+	c_program_text= """
+		struct A{ i32 &mut x; }
+		struct B{ A   &imut a; }
+		struct BWrapper{ B b; }
+		fn Foo()
+		{
+			var i32 mut x= 0;
+			var A a{ .x= x };
+			var B b{ .a= a };
+			var BWrapper b_wrapper{ .b{ .a= a } };
+			var BWrapper& b_wrapper_ref= b_wrapper;
+			var B& b_ref= b_wrapper_ref.b;
+
+			var A& a0= b_ref.a; // "a0.x" points to "a.x".
+			var A& a1= b_ref.a; // Error - creating second node pointing to "a.x" inside "a1".
+		}
+	"""
+	errors_list= ConvertErrors( tests_lib.build_program_with_errors( c_program_text ) )
+	assert( len(errors_list) > 0 )
+	assert( not HasError( errors_list, "ReferenceProtectionError", 14 ) )
+	assert( HasError( errors_list, "ReferenceProtectionError", 15 ) )
