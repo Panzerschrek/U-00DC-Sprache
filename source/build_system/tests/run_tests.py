f41978b2fc86f7bf8ee625b14d42b78b59b9fbd9
Panzerschrek <AntiPanzerschrek@yandex.ru>
2024-11-22 11:27:44 +0300

    Dobavlen test na otsutstvije fajla sborocnogo skripta.

--- source/build_system/tests/run_tests.py
+++ source/build_system/tests/run_tests.py
@@ -22,6 +22,16 @@ def RunBuildSystem( project_subdirectory ):
 	subprocess.check_call( build_system_args )
 
 
+# Returns subprocess result.
+def RunBuildSystemWithErrors( project_subdirectory ):
+	project_root = os.path.join( g_tests_path, project_subdirectory )
+	build_root = os.path.join( g_tests_build_root_path, project_subdirectory );
+	build_system_args= [ g_build_system_executable, "-q", "--compiler-executable", g_compiler_executable, "--build-system-imports-path", g_build_system_imports_path, "--ustlib-path", g_ustlib_path, "--project-directory", project_root, "--build-directory", build_root ]
+
+	# Run the build.
+	return subprocess.run( build_system_args, stderr=subprocess.PIPE )
+
+
 def RunExecutable( project_subdirectory, executable_name ):
 	subprocess.check_call( [ os.path.join( os.path.join( g_tests_build_root_path, project_subdirectory ), executable_name ) ], stdout= subprocess.DEVNULL )
 
@@ -49,6 +59,14 @@ def ТестЮникода():
 	# Non-ASCII project directory name, non-ASCII target name with spaces.
 	RunBuildSystem( "Тест Юникода" )
 	RunExecutable( "Тест Юникода", "Юникодный исполняемый файл - ☦ (православный)" )
+
+
+def MissingBuildFile():
+	# A directory with no build file.
+	res = RunBuildSystemWithErrors( "missing_build_file" )
+	stderr = str(res.stderr)
+	assert( stderr.find( "Compiler execution failed" ) != -1 )
+	assert( stderr.find( "Failed to load/build the build script shared library" ) != -1 )
 
 
 #
@@ -86,7 +104,7 @@ def main():
 	g_ustlib_path= args.ustlib_path
 
 
-	test_funcs = [ HelloWorldTest, TwoFilesExeTest, TwoTargetsTest, ТестЮникода ]
+	test_funcs = [ HelloWorldTest, TwoFilesExeTest, TwoTargetsTest, ТестЮникода, MissingBuildFile ]
 	print( "Run " + str(len(test_funcs)) + " Bürokratie tests" )
 
 	tests_passed= 0
