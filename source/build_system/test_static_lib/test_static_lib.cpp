9ea03a826a48236d4a269c87b5b1a09b165e1a76
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-01-01 12:55:29 +0300

    V teste ispoljzovanija "host" zavisimosti generirujem zagolovocnyj fajl pri pomosci utility, sobrannoj pod "host".

--- source/build_system/test_static_lib/test_static_lib.cpp
+++ source/build_system/test_static_lib/test_static_lib.cpp
@@ -1,6 +1,30 @@
 #include <cstdint>
+#include <cstddef>
+#include <cstdio>
 
 extern "C" uint32_t StaticLibFunc( const uint32_t x, const uint32_t y )
 {
 	return x * x + 3 * y;
+}
+
+extern "C" bool CreateFileWithContents(
+	const char* const name_null_terminated,
+	const std::byte* const contents,
+	const size_t contents_size )
+{
+	const auto f= std::fopen( name_null_terminated, "wb" );
+	if( f == nullptr )
+		return false;
+
+	// TODO - read in loop.
+	const size_t written= std::fwrite( contents, 1, contents_size, f );
+	if( written != contents_size )
+	{
+		std::fclose(f);
+		return false;
+	}
+
+	std::fclose(f);
+
+	return true;
 }
