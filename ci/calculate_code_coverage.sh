6253a6ee54fd0f2f7ce8d2d5fb4511844e09ae58
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-01-03 14:57:17 +0300

    Pravka skripta sborki s podscötom pokrytija testami.

--- ci/calculate_code_coverage.sh
+++ ci/calculate_code_coverage.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Get llvm
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.6/clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04.tar.xz &&\
-xz -d clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04.tar.xz &&\
-tar -xf clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04.tar &&\
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.6/clang+llvm-17.0.6-x86_64-linux-gnu-ubuntu-22.04.tar.xz &&\
+xz -d clang+llvm-17.0.6-x86_64-linux-gnu-ubuntu-22.04.tar.xz &&\
+tar -xf clang+llvm-17.0.6-x86_64-linux-gnu-ubuntu-22.04.tar &&\
 \
 # Configure build
 mkdir build_dir &&\
 cd build_dir &&\
 # Build only tests - do not count coverage from later compiler0 launches.
-cmake ../source/ -G Ninja -DCMAKE_BUILD_TYPE=Debug -DLLVM_LIB_DIR=$PWD/../clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04/lib/ -DU_BUILD_COMPILER=NO -DU_BUILD_DOCS=NO -DU_COVERAGE_ENABLED=YES &&\
+cmake ../source/ -G Ninja -DCMAKE_BUILD_TYPE=Debug -DLLVM_LIB_DIR=$PWD/../clang+llvm-17.0.6-x86_64-linux-gnu-ubuntu-22.04/lib/ -DU_BUILD_COMPILER=NO -DU_BUILD_DOCS=NO -DU_COVERAGE_ENABLED=YES &&\
 \
 cmake --build . &&\
 \
