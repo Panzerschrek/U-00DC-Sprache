5b220f006446dad85b20809347985da4eb4d4234
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-01-05 12:52:03 +0300

    Adaptaçija dopolniteljnyh "CI" sborok pod "Windows" k biblioteke "LLVM" versii 17.

--- ci/build_debug_windows.ps1
+++ ci/build_debug_windows.ps1
@@ -3,18 +3,18 @@ $curDir = Get-Location
 $curDir = $curDir -replace '\\', '/'
 
 # Obtain LLVM
-Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.7/llvm-15.0.7.src.tar.xz" -OutFile "llvm-15.0.7.src.tar.xz"
-Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.7/cmake-15.0.7.src.tar.xz" -OutFile "cmake-15.0.7.src.tar.xz"
-7z x llvm-15.0.7.src.tar.xz
-7z x llvm-15.0.7.src.tar
-7z x cmake-15.0.7.src.tar.xz
-7z x cmake-15.0.7.src.tar
+Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.6/llvm-17.0.6.src.tar.xz" -OutFile "llvm-17.0.6.src.tar.xz"
+Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.6/cmake-17.0.6.src.tar.xz" -OutFile "cmake-17.0.6.src.tar.xz"
+7z x llvm-17.0.6.src.tar.xz
+7z x llvm-17.0.6.src.tar
+7z x cmake-17.0.6.src.tar.xz
+7z x cmake-17.0.6.src.tar
 
 # Delete archives
-del llvm-15.0.7.src.tar.xz
-del llvm-15.0.7.src.tar
-del cmake-15.0.7.src.tar.xz
-del cmake-15.0.7.src.tar
+del llvm-17.0.6.src.tar.xz
+del llvm-17.0.6.src.tar
+del cmake-17.0.6.src.tar.xz
+del cmake-17.0.6.src.tar
 
 # Messy stuff. Call vcvarsall.bat, extract all environment variable prepared in this call and redefine them in context of this powershell script.
 cmd /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat`" amd64 && set > %temp%\vcvars.txt"
@@ -26,7 +26,7 @@ Get-Content "$env:temp\vcvars.txt" | Foreach-Object {
 
 # Perform cmake preparation
 mkdir build_dir
-cmake -S source -B build_dir  -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MODULE_PATH="$curDir/cmake-15.0.7.src/Modules/" -DLLVM_SRC_DIR="$curDir/llvm-15.0.7.src/" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_NATIVE_ARCH="X86" -DLLVM_BUILD_BENCHMARKS=OFF -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_BUILD_DOCS=OFF -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_BUILD_TESTS=OFF -DU_BUILD_COMPILER2=ON -DU_BUILD_COMPILER3=OFF -DU_BUILD_PY_TESTS=OFF -DU_BUILD_UNICODE_FILE_NAMES_TEST=OFF
+cmake -S source -B build_dir  -G "Ninja" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_MODULE_PATH="$curDir/cmake-17.0.6.src/Modules/" -DLLVM_SRC_DIR="$curDir/llvm-17.0.6.src/" -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_NATIVE_ARCH="X86" -DLLVM_BUILD_BENCHMARKS=OFF -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_BUILD_DOCS=OFF -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_BUILD_TESTS=OFF -DLLVM_INCLUDE_TESTS=OFF -DU_BUILD_COMPILER2=ON -DU_BUILD_COMPILER3=OFF -DU_BUILD_PY_TESTS=OFF -DU_BUILD_UNICODE_FILE_NAMES_TEST=OFF
 
 # Since there is not enought space on Github hosted action runners, run builds one by one and than delete some huge files
 
--- ci/build_release_windows_32.ps1
+++ ci/build_release_windows_32.ps1
@@ -3,24 +3,27 @@ $curDir = Get-Location
 $curDir = $curDir -replace '\\', '/'
 
 # Obtain LLVM
-Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.7/llvm-15.0.7.src.tar.xz" -OutFile "llvm-15.0.7.src.tar.xz"
-Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.7/cmake-15.0.7.src.tar.xz" -OutFile "cmake-15.0.7.src.tar.xz"
-Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.7/clang-15.0.7.src.tar.xz" -OutFile "clang-15.0.7.src.tar.xz"
-Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.7/lld-15.0.7.src.tar.xz" -OutFile "lld-15.0.7.src.tar.xz"
-Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.7/libunwind-15.0.7.src.tar.xz" -OutFile "libunwind-15.0.7.src.tar.xz"
-7z x llvm-15.0.7.src.tar.xz
-7z x llvm-15.0.7.src.tar
-7z x cmake-15.0.7.src.tar.xz
-7z x cmake-15.0.7.src.tar
-7z x clang-15.0.7.src.tar.xz
-7z x clang-15.0.7.src.tar
-7z x lld-15.0.7.src.tar.xz
-7z x lld-15.0.7.src.tar
-7z x libunwind-15.0.7.src.tar.xz
-7z x libunwind-15.0.7.src.tar
+Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.6/llvm-17.0.6.src.tar.xz" -OutFile "llvm-17.0.6.src.tar.xz"
+Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.6/cmake-17.0.6.src.tar.xz" -OutFile "cmake-17.0.6.src.tar.xz"
+Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.6/clang-17.0.6.src.tar.xz" -OutFile "clang-17.0.6.src.tar.xz"
+Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.6/lld-17.0.6.src.tar.xz" -OutFile "lld-17.0.6.src.tar.xz"
+Invoke-WebRequest -Uri "https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.6/libunwind-17.0.6.src.tar.xz" -OutFile "libunwind-17.0.6.src.tar.xz"
+7z x llvm-17.0.6.src.tar.xz
+7z x llvm-17.0.6.src.tar
+7z x cmake-17.0.6.src.tar.xz
+7z x cmake-17.0.6.src.tar
+7z x clang-17.0.6.src.tar.xz
+7z x clang-17.0.6.src.tar
+7z x lld-17.0.6.src.tar.xz
+7z x lld-17.0.6.src.tar
+7z x libunwind-17.0.6.src.tar.xz
+7z x libunwind-17.0.6.src.tar
+
+# LLVM includes its cmake modules using path like "../cmake/Modules/CMakePolicy.cmake". So, reaname this directory.
+Rename-Item -path "cmake-17.0.6.src" -NewName "cmake"
 
 # lld Mach-O library includes libunwind using path relative to llvm src directory and without a version. So, rename it.
-Rename-Item -path "libunwind-15.0.7.src" -NewName "libunwind"
+Rename-Item -path "libunwind-17.0.6.src" -NewName "libunwind"
 
 # Messy stuff. Call vcvarsall.bat, extract all environment variable prepared in this call and redefine them in context of this powershell script.
 cmd /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat`" amd64_x86 && set > %temp%\vcvars.txt"
@@ -32,5 +35,5 @@ Get-Content "$env:temp\vcvars.txt" | Foreach-Object {
 
 # Perform cmake preparation and build
 mkdir build_dir
-cmake -S source -B build_dir  -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH="$curDir/cmake-15.0.7.src/Modules/" -DLLVM_SRC_DIR="$curDir/llvm-15.0.7.src/" -DLLVM_EXTERNAL_CLANG_SOURCE_DIR="$curDir/clang-15.0.7.src/" -DLLVM_EXTERNAL_LLD_SOURCE_DIR="$curDir/lld-15.0.7.src/" -DLLVM_TOOL_LLD_BUILD=ON -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_NATIVE_ARCH="X86" -DLLVM_BUILD_BENCHMARKS=OFF -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_BUILD_DOCS=OFF -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_BUILD_TESTS=OFF -DU_BUILD_COMPILER2=ON -DU_BUILD_COMPILER3=ON -DU_BUILD_CPP_HEADER_CONVERTER=ON -DU_BUILD_INTERPRETER=OFF -DU_BUILD_PY_TESTS=OFF -DU_BUILD_BUILD_SYSTEM_TESTS=OFF -DU_BUILD_UNICODE_FILE_NAMES_TEST=OFF
+cmake -S source -B build_dir  -G "Ninja" -DCMAKE_BUILD_TYPE=Release -DCMAKE_MODULE_PATH="$curDir/cmake/Modules/" -DLLVM_SRC_DIR="$curDir/llvm-17.0.6.src/" -DLLVM_EXTERNAL_CLANG_SOURCE_DIR="$curDir/clang-17.0.6.src/" -DLLVM_EXTERNAL_LLD_SOURCE_DIR="$curDir/lld-17.0.6.src/" -DLLVM_TOOL_LLD_BUILD=ON -DLLVM_TARGETS_TO_BUILD="X86" -DLLVM_NATIVE_ARCH="X86" -DLLVM_BUILD_BENCHMARKS=OFF -DLLVM_INCLUDE_BENCHMARKS=OFF -DLLVM_BUILD_DOCS=OFF -DLLVM_BUILD_EXAMPLES=OFF -DLLVM_INCLUDE_TESTS=OFF -DLLVM_BUILD_TESTS=OFF -DU_BUILD_COMPILER2=ON -DU_BUILD_COMPILER3=ON -DU_BUILD_CPP_HEADER_CONVERTER=ON -DU_BUILD_INTERPRETER=OFF -DU_BUILD_PY_TESTS=OFF -DU_BUILD_BUILD_SYSTEM_TESTS=OFF -DU_BUILD_UNICODE_FILE_NAMES_TEST=OFF
 cmake --build build_dir
