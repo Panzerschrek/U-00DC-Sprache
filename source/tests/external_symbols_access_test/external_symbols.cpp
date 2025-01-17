c6ff20034396f34fc61981c5ac3cedc23eb2a43b
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-01-17 12:28:03 +0300

    Testirujem, cto dostup k globaljnym peremennym, objavlennym v "C++" kode, realjno rabotajet.

--- source/tests/external_symbols_access_test/external_symbols.cpp
+++ source/tests/external_symbols_access_test/external_symbols.cpp
@@ -9,3 +9,12 @@ extern "C" uint64_t if_coro_advance( const uint64_t x, const uint64_t y )
 {
 	return x - y;
 }
+
+extern const float __cpp_global_constant= 2.375f;
+
+uint64_t generator= 77u;
+
+extern "C" uint64_t GetGeneratorValue()
+{
+	return generator;
+}
