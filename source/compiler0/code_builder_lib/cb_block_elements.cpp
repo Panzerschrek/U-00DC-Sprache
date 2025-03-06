d922ec33872aec18f108bf87fc98b0ef03917415
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-03-06 14:48:45 +0300

    Udalena nenužnaja proverka.

--- source/compiler0/code_builder_lib/cb_block_elements.cpp
+++ source/compiler0/code_builder_lib/cb_block_elements.cpp
@@ -3230,13 +3230,6 @@ void CodeBuilder::BuildDisassemblyDeclarationComponentImpl(
 		return;
 	}
 
-	if( function_context.variables_state.NodeMoved( initializer_variable ) )
-	{
-		// TODO - do we need such check?
-		REPORT_ERROR( AccessingMovedVariable, names_scope.GetErrors(), component.src_loc, initializer_variable->name );
-		return;
-	}
-
 	if( component.mutability_modifier == MutabilityModifier::Constexpr && initializer_variable->constexpr_value == nullptr )
 		REPORT_ERROR( VariableInitializerIsNotConstantExpression, names_scope.GetErrors(), component.src_loc );
 
@@ -3304,13 +3297,6 @@ void CodeBuilder::BuildDisassemblyDeclarationComponentImpl(
 	{
 		// Allow disassembling only immediate values.
 		REPORT_ERROR( ImmediateValueExpectedInDisassemblyDeclaration, names_scope.GetErrors(), component.src_loc );
-		return;
-	}
-
-	if( function_context.variables_state.NodeMoved( variable ) )
-	{
-		// TODO - do we need such check?
-		REPORT_ERROR( AccessingMovedVariable, names_scope.GetErrors(), component.src_loc, variable->name );
 		return;
 	}
 
@@ -3389,13 +3375,6 @@ void CodeBuilder::BuildDisassemblyDeclarationComponentImpl(
 	{
 		// Allow disassembling only immediate values.
 		REPORT_ERROR( ImmediateValueExpectedInDisassemblyDeclaration, names_scope.GetErrors(), component.src_loc );
-		return;
-	}
-
-	if( function_context.variables_state.NodeMoved( variable ) )
-	{
-		// TODO - do we need such check?
-		REPORT_ERROR( AccessingMovedVariable, names_scope.GetErrors(), component.src_loc, variable->name );
 		return;
 	}
 
