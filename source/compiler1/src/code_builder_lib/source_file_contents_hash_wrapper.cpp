edf135c4902f990acb107b77103d3d5e1fb52105
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-04-21 10:35:03 +0300

    Pocinka sborki Kompilätora1.

--- source/compiler1/src/code_builder_lib/source_file_contents_hash_wrapper.cpp
+++ source/compiler1/src/code_builder_lib/source_file_contents_hash_wrapper.cpp
@@ -1,4 +1,4 @@
-#include "../../../code_builder_lib_common/source_file_contents_hash.hpp"
+#include "../../../code_builder_lib_common/long_stable_hash.hpp"
 
 // If contents of this file changed, source_file_contents_hash_wrapper.uh must be changed too!
 
@@ -12,6 +12,6 @@ extern "C" void U1_CalculateSourceFileContentsHash(
 	const U1_CalculateSourceFileContentsHashCallback callback,
 	const UserHandle callback_data )
 {
-	const std::string hash= U::CalculateSourceFileContentsHash( std::string_view( data, size ) );
+	const std::string hash= U::CalculateLongStableHash( std::string_view( data, size ) );
 	callback( callback_data, hash.data(), hash.size() );
 }
