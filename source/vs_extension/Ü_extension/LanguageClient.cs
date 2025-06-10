939d2fef77c8d3cf95b1c6b563e372e7eb335391
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-06-10 19:06:34 +0300

    Modifikaçii jazykovogo klijena plagina dlä "Visual Studio", ctoby on hotj kak-to rabotal.

--- source/vs_extension/Ü_extension/LanguageClient.cs
+++ source/vs_extension/Ü_extension/LanguageClient.cs
@@ -29,6 +29,11 @@ namespace Ü_extension
 		public event AsyncEventHandler<EventArgs> StartAsync;
 		public event AsyncEventHandler<EventArgs> StopAsync;
 
+		private Process process_ = null;
+		Connection connection_ = null;
+		Stream read_stream_ = null;
+		Stream write_stream_ = null;
+
 		public async Task<Connection> ActivateAsync(CancellationToken token)
 		{
 			await Task.Yield();
@@ -38,16 +43,22 @@ namespace Ü_extension
 			info.Arguments = "--log-file C:/Users/user/Documents/Projects/other/U-00DC-Sprache/other/vs_extension_language_server.log";
 			info.RedirectStandardInput = true;
 			info.RedirectStandardOutput = true;
+			info.RedirectStandardError = true;
 			info.UseShellExecute = false;
 			info.CreateNoWindow = true;
-			info.StandardOutputEncoding = Encoding.UTF8;
+			//info.StandardOutputEncoding = Encoding.UTF8;
+			//info.StandardErrorEncoding = Encoding.UTF8;
 
-			Process process = new Process();
-			process.StartInfo = info;
+			process_ = new Process();
+			process_.StartInfo = info;
 
-			if (process.Start())
+			if (process_.Start())
 			{
-				return new Connection(process.StandardOutput.BaseStream, process.StandardInput.BaseStream);
+				read_stream_ = process_.StandardOutput.BaseStream;
+				write_stream_ = process_.StandardInput.BaseStream;
+
+				connection_ = new Connection(read_stream_, write_stream_);
+				return connection_;
 			}
 
 			return null;
