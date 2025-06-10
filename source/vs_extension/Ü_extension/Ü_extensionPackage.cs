0dbe3c2ed675415c5236c39426b9abd5090fe256
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-06-10 08:40:36 +0300

    Dobavlena testovaja komanda.

--- source/vs_extension/Ü_extension/Ü_extensionPackage.cs
+++ source/vs_extension/Ü_extension/Ü_extensionPackage.cs
@@ -25,6 +25,7 @@ namespace Ü_extension
 	/// </remarks>
 	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
 	[Guid(Ü_extensionPackage.PackageGuidString)]
+	[ProvideMenuResource("Menus.ctmenu", 1)]
 	public sealed class Ü_extensionPackage : AsyncPackage
 	{
 		/// <summary>
@@ -46,6 +47,7 @@ namespace Ü_extension
 			// When initialized asynchronously, the current thread may be a background thread at this point.
 			// Do any initialization that requires the UI thread after switching to the UI thread.
 			await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+		    await LanguageInfoCommand.InitializeAsync(this);
 		}
 
 		#endregion
