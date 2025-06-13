505ca04aee216ecda4412fe0a2688fddf0559da3
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-06-13 12:57:50 +0300

    Sozdan skript sborki.

---
+++ source/visual_studio_extension/build.bat
@@ -0,0 +1,3 @@
+:: C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin
+msbuild -restore /property:Configuration=Debug
+msbuild -restore /property:Configuration=Release
