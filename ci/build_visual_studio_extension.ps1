bdb16c6a57d57da9ac93f12bbe09309c1b38034f
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-06-13 15:24:20 +0300

    Ždöm, poka proçessy zaveršatsä.

--- ci/build_visual_studio_extension.ps1
+++ ci/build_visual_studio_extension.ps1
@@ -1,3 +1,3 @@
 Set-Location -path source/visual_studio_extension
-Start-Process -FilePath "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" -ArgumentList "-restore /property:Configuration=Debug"
-Start-Process -FilePath "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" -ArgumentList "-restore /property:Configuration=Release"
+Start-Process -FilePath "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" -Wait -ArgumentList "-restore /property:Configuration=Debug"
+Start-Process -FilePath "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" -Wait -ArgumentList "-restore /property:Configuration=Release"
