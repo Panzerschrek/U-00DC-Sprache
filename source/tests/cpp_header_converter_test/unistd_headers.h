1fd1bfbb209a3f70f6cbbe0acb25069498a6979c
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-07-11 12:50:02 +0300

    S çelju otladki vyvodim v log sborki soderžimoje fajla, polucennogo putöm preobrazovanija zagolovocnyh fajlov "Unix".

--- source/tests/cpp_header_converter_test/unistd_headers.h
+++ source/tests/cpp_header_converter_test/unistd_headers.h
@@ -1,2 +1,20 @@
 #pragma once
+
+#include <signal.h>
+#include <poll.h>
+
 #include <unistd.h>
+#include <spawn.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <errno.h>
+
+#include <dlfcn.h>
+
+#include <sys/wait.h>
+#include <time.h>
+
+#include <dirent.h>
