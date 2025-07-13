96f4f194f5ee68e3ef13f4cfd297f96a0ef8190a
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-07-13 08:31:32 +0300

    Revert "S çelju otladki vyvodim v log sborki soderžimoje fajla, polucennogo putöm preobrazovanija zagolovocnyh fajlov "Unix"."
    This reverts commit 1fd1bfbb209a3f70f6cbbe0acb25069498a6979c.

--- source/tests/cpp_header_converter_test/unistd_headers.h
+++ source/tests/cpp_header_converter_test/unistd_headers.h
@@ -1,20 +1,2 @@
 #pragma once
-
-#include <signal.h>
-#include <poll.h>
-
 #include <unistd.h>
-#include <spawn.h>
-
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-#include <errno.h>
-
-#include <dlfcn.h>
-
-#include <sys/wait.h>
-#include <time.h>
-
-#include <dirent.h>
