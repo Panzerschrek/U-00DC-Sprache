dd789d4a69c9c5edd934a3033d4fc4b4f66f5e30
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-07-25 11:10:32 +0300

    Dobavleny testy na peredacu v "C" kod bazovyh çelyh cisel.

--- source/tests/c_calling_convention_test/c_calling_convention_test.cpp
+++ source/tests/c_calling_convention_test/c_calling_convention_test.cpp
@@ -6,39 +6,34 @@
 extern "C"
 {
 
-void Pass_i8_Test0( const int8_t x )
-{
-	TEST_ASSERT( x == 27 );
-}
-
-void Pass_i8_Test1( const int8_t x )
-{
-	TEST_ASSERT( x == -58 );
-}
-
-void Pass_u8_Test0( const uint8_t x )
-{
-	TEST_ASSERT( x == 117 );
-}
-
-void Pass_u8_Test1( const uint8_t x )
-{
-	TEST_ASSERT( x == 134 );
-}
-
-void Pass_u8_Test2( const uint8_t x )
-{
-	TEST_ASSERT( x == 249 );
-}
-
-void Pass_i16_Test0( const int16_t x )
-{
-	TEST_ASSERT( x == 27346 );
-}
-
-void Pass_i16_Test1( const int16_t x )
-{
-	TEST_ASSERT( x == -15343 );
-}
+void Pass_bool_Test0( const bool x ) { TEST_ASSERT( x == false ); }
+void Pass_bool_Test1( const bool x ) { TEST_ASSERT( x == true ); }
+void Pass_i8_Test0( const int8_t x ) { TEST_ASSERT( x == 27 ); }
+void Pass_i8_Test1( const int8_t x ) { TEST_ASSERT( x == -58 ); }
+void Pass_u8_Test0( const uint8_t x ) { TEST_ASSERT( x == 117 ); }
+void Pass_u8_Test1( const uint8_t x ) { TEST_ASSERT( x == 134 ); }
+void Pass_u8_Test2( const uint8_t x ) { TEST_ASSERT( x == 249 ); }
+void Pass_i16_Test0( const int16_t x ) { TEST_ASSERT( x == 27346 ); }
+void Pass_i16_Test1( const int16_t x ) { TEST_ASSERT( x == -15343 ); }
+void Pass_u16_Test0( const uint16_t x ) { TEST_ASSERT( x == 17 ); }
+void Pass_u16_Test1( const uint16_t x ) { TEST_ASSERT( x == 15642 ); }
+void Pass_u16_Test2( const uint16_t x ) { TEST_ASSERT( x == 30651 ); }
+void Pass_u16_Test3( const uint16_t x ) { TEST_ASSERT( x == 52188 ); }
+void Pass_i32_Test0( const int32_t x ) { TEST_ASSERT( x == 274383 ); }
+void Pass_i32_Test1( const int32_t x ) { TEST_ASSERT( x == -7456 ); }
+void Pass_i32_Test2( const int32_t x ) { TEST_ASSERT( x == 0x78ABCDEF ); }
+void Pass_i32_Test3( const int32_t x ) { TEST_ASSERT( x == -674348993 ); }
+void Pass_u32_Test0( const uint32_t x ) { TEST_ASSERT( x == 78u ); }
+void Pass_u32_Test1( const uint32_t x ) { TEST_ASSERT( x == 45677u ); }
+void Pass_u32_Test2( const uint32_t x ) { TEST_ASSERT( x == 6633477u ); }
+void Pass_u32_Test3( const uint32_t x ) { TEST_ASSERT( x == 0xFEDCBA98u ); }
+void Pass_i64_Test0( const int64_t x ) { TEST_ASSERT( x == 636746ll ); }
+void Pass_i64_Test1( const int64_t x ) { TEST_ASSERT( x == -36ll ); }
+void Pass_i64_Test2( const int64_t x ) { TEST_ASSERT( x == -6433763852258913ll ); }
+void Pass_i64_Test3( const int64_t x ) { TEST_ASSERT( x == 7434744889515923245ll ); }
+void Pass_u64_Test0( const uint64_t x ) { TEST_ASSERT( x == 7612ull ); }
+void Pass_u64_Test1( const uint64_t x ) { TEST_ASSERT( x == 2147521472ull ); }
+void Pass_u64_Test2( const uint64_t x ) { TEST_ASSERT( x == 7445889504678477554ull ); }
+void Pass_u64_Test3( const uint64_t x ) { TEST_ASSERT( x == 18246784073809531617ull ); }
 
 } // extern "C"
