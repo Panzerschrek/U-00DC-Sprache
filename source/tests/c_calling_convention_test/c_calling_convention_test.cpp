38fb1741121fe76eca8ed5729cfb8c174c636eb2
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-07-25 10:36:32 +0300

    Dobavleny jescö koje-kakije testy. Vnezapno obnaružilsoj, cto peredaca znacenij tipa "i16" v "C" kod ne rabotajet kak nado - netu atributa "signext".

--- source/tests/c_calling_convention_test/c_calling_convention_test.cpp
+++ source/tests/c_calling_convention_test/c_calling_convention_test.cpp
@@ -6,9 +6,39 @@
 extern "C"
 {
 
-void PassI8_Test0( const int8_t x )
+void Pass_i8_Test0( const int8_t x )
 {
 	TEST_ASSERT( x == 27 );
+}
+
+void Pass_i8_Test1( const int8_t x )
+{
+	TEST_ASSERT( x == -58 );
+}
+
+void Pass_u8_Test0( const uint8_t x )
+{
+	TEST_ASSERT( x == 117 );
+}
+
+void Pass_u8_Test1( const uint8_t x )
+{
+	TEST_ASSERT( x == 134 );
+}
+
+void Pass_u8_Test2( const uint8_t x )
+{
+	TEST_ASSERT( x == 249 );
+}
+
+void Pass_i16_Test0( const int16_t x )
+{
+	TEST_ASSERT( x == 27346 );
+}
+
+void Pass_i16_Test1( const int16_t x )
+{
+	TEST_ASSERT( x == -15343 );
 }
 
 } // extern "C"
