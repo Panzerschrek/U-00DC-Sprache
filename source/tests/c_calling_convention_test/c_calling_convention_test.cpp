807d3b6515c4050fff9ccc54ae6408d4bba61da7
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-07-28 10:54:48 +0300

    Napisany testy na vozvrat kortežej iz "C" koda v Ü s dvumä çelocislennymi i odnim vescestvennym elementom.

--- source/tests/c_calling_convention_test/c_calling_convention_test.cpp
+++ source/tests/c_calling_convention_test/c_calling_convention_test.cpp
@@ -1933,6 +1933,18 @@ Tuple3<uint8_t, uint64_t, uint8_t> Get_tup_u8_u64_u8_Test0() { return { 0xAB, 0x
 Tuple3<uint16_t, uint32_t, uint16_t> Get_tup_u16_u32_u16_Test0() { return { 0x0123, 0x456789AB, 0xCDEF }; }
 Tuple3<uint16_t, uint64_t, uint16_t> Get_tup_u16_u64_u16_Test0() { return { 0xFEDC, 0x17283A4B5C6D7E8F, 0x9876 }; }
 Tuple3<uint32_t, uint64_t, uint32_t> Get_tup_u32_u64_u32_Test0() { return { 0x01234567, 0x17283A4B5C6D7E8F, 0x89ABCEDF }; }
+Tuple3<float, int32_t, int32_t> Get_tup_f32_i32_i32_Test0() { return { 123.45f, 266747477, -963237321 }; }
+Tuple3<int32_t, float, int32_t> Get_tup_i32_f32_i32_Test0() { return { -196323732, 236.5f, 266745477  }; }
+Tuple3<int32_t, int32_t, float> Get_tup_i32_i32_f32_Test0() { return { 196323735, 166745427, -0.7f }; }
+Tuple3<float, uint64_t, uint64_t> Get_tup_f32_u64_u64_Test0() { return { 323.25f, 0x64AB3C5482367DE3u, 0x17283A4B5C6D7E8Fu }; }
+Tuple3<uint64_t, float, uint64_t> Get_tup_u64_f32_u64_Test0() { return { 0x7637347A36B4E218u, 1336.5f, 0x067374735AE7DFC13u }; }
+Tuple3<uint64_t, uint64_t, float> Get_tup_u64_u64_f32_Test0() { return { 0x27283A4B5C637E8Fu, 0xE637347436B47218u, 4.7f }; }
+Tuple3<double, int32_t, int32_t> Get_tup_f64_i32_i32_Test0() { return { 123.45, 266747477, -963237321 }; }
+Tuple3<int32_t, double, int32_t> Get_tup_i32_f64_i32_Test0() { return { -196323732, 236.5, 266745477 }; }
+Tuple3<int32_t, int32_t, double> Get_tup_i32_i32_f64_Test0() { return { 196323735, 166745427, -0.7 }; }
+Tuple3<double, uint64_t, uint64_t> Get_tup_f64_u64_u64_Test0() { return { 323.25, 0x64AB3C5482367DE3u, 0x17283A4B5C6D7E8Fu }; }
+Tuple3<uint64_t, double, uint64_t> Get_tup_u64_f64_u64_Test0() { return { 0x7637347A36B4E218u, 1336.5, 0x067374735AE7DFC13u }; }
+Tuple3<uint64_t, uint64_t, double> Get_tup_u64_u64_f64_Test0() { return { 0x27283A4B5C637E8Fu, 0xE637347436B47218u, 4.7 }; }
 
 #pragma GCC diagnostic pop
 
