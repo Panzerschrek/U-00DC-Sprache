a04f292fa51ad2b4b21de1181438cb07fcba9708
Panzerschrek <AntiPanzerschrek@yandex.ru>
2025-09-01 16:57:52 +0300

    Dopolnenije i ispravlenije kommentarijev.

--- source/compiler0/code_builder_lib/cb_coroutines.cpp
+++ source/compiler0/code_builder_lib/cb_coroutines.cpp
@@ -718,7 +718,7 @@ Value CodeBuilder::BuildAwait( NamesScope& names_scope, FunctionContext& functio
 	if( async_func_variable->type == invalid_type_ )
 		return ErrorValue();
 
-	// Check for "non-sync" variables existance after expression evaluation but befor suspension.
+	// Check for "non-sync" variables existance after expression evaluation but before suspension.
 	CheckSyncCoroutineHasNoNonSyncLocalVariablesAtSuspensionPoint( names_scope, function_context, src_loc );
 
 	if( async_func_variable->value_type != ValueType::Value )
