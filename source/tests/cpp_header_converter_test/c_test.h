abbb115cb4e1081c56d8f43454c0ed31e986d193
Panzerschrek <AntiPanzerschrek@yandex.ru>
2026-05-26 21:09:26 +0300

    Dobavlen jescö odin test.

--- source/tests/cpp_header_converter_test/c_test.h
+++ source/tests/cpp_header_converter_test/c_test.h
@@ -412,6 +412,7 @@ typedef unsigned long long int __64_int_from_c;
 
 // Should translate define - alias for a function.
 #define FUNCTION_ZERO_ARGS Function_ZeroArgs
+#define FUNCTION_ZERO_ARGS_COPY FUNCTION_ZERO_ARGS
 
 // Should translate define - alias for a variable define.
 #define UNSIGNED_CONSTANT_COPY UNSIGNED_CONSTANT
