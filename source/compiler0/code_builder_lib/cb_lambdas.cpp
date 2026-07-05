88c82126595fbd3445b768da86d51f547351f950
Panzerschrek <AntiPanzerschrek@yandex.ru>
2026-07-05 17:57:20 +0300

    Don't create reference pollution for destination being a captured variable in a "byval this" lambda. It's impossible and unnecessary.

--- source/compiler0/code_builder_lib/cb_lambdas.cpp
+++ source/compiler0/code_builder_lib/cb_lambdas.cpp
@@ -641,6 +641,16 @@ ClassPtr CodeBuilder::PrepareLambdaClass( NamesScope& names_scope, FunctionConte
 				result_pollution.dst= *dst_param_reference;
 			else if( const auto dst_variable_ptr= std::get_if<VariablePtr>( &pollution.dst ) )
 			{
+				const bool is_byval_this_lambda= lambda.function.type.params.front().reference_modifier == Synt::ReferenceModifier::None;
+				if( is_byval_this_lambda )
+				{
+					// Ignore reference pollution for destination being a captured variable in "byval this" lambdas.
+					// For variables captured by value this basically means pollution for value argument, which isn't allowed.
+					// For variables captured by reference this means pollition of second order inner reference,
+					// which is also not allowed and shold be checked prior to this place.
+					continue;
+				}
+
 				if( const auto it= captured_variable_to_lambda_inner_reference_tag.find( *dst_variable_ptr ); it != captured_variable_to_lambda_inner_reference_tag.end() )
 					result_pollution.dst= std::make_pair( this_param_index, it->second );
 				else
